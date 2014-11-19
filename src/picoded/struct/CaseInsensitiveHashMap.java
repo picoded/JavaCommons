1c6686c8cbc2d44e111b73880185859454ad614f
Eugene Cheah <eugene@socialoctet.com>
2014-11-19 04:51:00 +0000

    Documentation tweaks

--- src/picoded/struct/CaseInsensitiveHashMap.java
+++ src/picoded/struct/CaseInsensitiveHashMap.java
@@ -1,8 +1,9 @@
 package picoded.struct;
 
+import java.util.Map;
 import java.util.HashMap;
 
-/// Case Insensitive HashMap, useful for various things (like Oracle/mysql compatibility)
+/// Case Insensitive HashMap, useful for various things.
 /// Normalizes, stores, and retrives all keys in lowercase.
 ///
 /// As this class extends HashMap directly, several of its common functionalities are inherited
@@ -26,35 +27,69 @@ public class CaseInsensitiveHashMap<K extends String,V>  extends HashMap<K,V> {
 	/// Java serialversion uid: http://stackoverflow.com/questions/285793/what-is-a-serialversionuid-and-why-should-i-use-it
 	private static final long serialVersionUID = 42L;
 	
-	
-	///
 	/// Associates the specified value with the specified key in this map.
 	/// If the map previously contained a mapping for the key, the old value is replaced.
 	///
-	/// @param   key     key with which the specified value is to be associated
+	/// @param   key     key string with which the specified value is to be associated
 	/// @param   value   value to be associated with the specified key
 	///
+	/// @returns  the previous value associated with key, or null if there was no mapping for key. 
+	///           (A null return can also indicate that the map previously associated null with key.)
+	@SuppressWarnings("unchecked")
+	@Override 
 	public V put(K key, V value) {
-		@Override @SuppressWarnings("unchecked")
 		return super.put( (K)(key.toString().toLowerCase()), value);
 	}
 	
+	/// Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
+	/// More formally, if this map contains a mapping from a key k to a value v such that (key==null ? k==null : key.toLowerCase().equals(k)), 
+	/// then this method returns v; otherwise it returns null. (There can be at most one such mapping.)
+	/// A return value of null does not necessarily indicate that the map contains no mapping for the key; 
+	/// it's also possible that the map explicitly maps the key to null. The containsKey operation may be used to distinguish these two cases.
+	///
+	/// @param    key     the key whose associated value is to be returned
+	///
+	/// @returns  the value to which the specified key is mapped, or null if this map contains no mapping for the key
+	@SuppressWarnings("unchecked")
+	@Override 
 	public V get(Object key) {
-		@Override @SuppressWarnings("unchecked")
 		return super.get( (K)(key.toString().toLowerCase()) );
 	}
 	
+	/// Returns true if this map contains a mapping for the specified key.
+	///
+	/// @param    key     The key whose presence in this map is to be tested
+	///
+	/// @returns  true if this map contains a mapping for the specified key.
+	@SuppressWarnings("unchecked")
+	@Override 
 	public boolean containsKey(Object key) {
-		@Override @SuppressWarnings("unchecked")
 		return super.containsKey( (K)(key.toString().toLowerCase()) );
 	}
 	
+	/// Removes the mapping for the specified key from this map if present.
+	///
+	/// @param    key     key whose mapping is to be removed from the map
+	///
+	/// @returns  the previous value associated with key, or null if there was no mapping for key. 
+	///           (A null return can also indicate that the map previously associated null with key.)
+	@SuppressWarnings("unchecked")
+	@Override 
 	public V remove(Object key) {
-		@Override @SuppressWarnings("unchecked")
 		return super.remove( (K)(key.toString().toLowerCase()) );
 	}
 	
-	/// TODO: putAll
-	// putAll(Map<? extends K,? extends V> m)
-	
+	/// @todo   the actual class implementation
+	///
+	/// Copies all of the mappings from the specified map to this map. 
+	/// These mappings will replace any mappings that this map had for any of the keys currently in the specified map.
+	///
+	/// Note: Care should be taken when importing multiple case sensitive mappings, as the order which they are
+	/// overwritten may not be predictable / in sequence.
+	///
+	/// @param    m     Original mappings to be stored in this map
+	@Override 
+	public void putAll(Map<? extends K,? extends V> m) {
+		throw new RuntimeException("Not Yet Implmented");
+	}
 }

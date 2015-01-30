c01b7b91bd7eb7cac99a20a2add6a40b79b54b6f
Eugene Cheah <eugene@picoded.com>
2015-01-31 02:29:00 +0800

    Experimental jSql transfer

--- src/picoded/jSql/db/package-info.java
+++ src/picoded/jSql/db/package-info.java
@@ -1,3 +1,3 @@
 /// Internal implementations of various databases for jSql. These packages file are NOT meant for public usage
 /// However access is made open, to facilitate prototyping of additional jSql extensions
-package picoded.jSql.dbInternals;
+package picoded.jSql.db;
--- src/picoded/jSql/package-info.java
+++ src/picoded/jSql/package-info.java
@@ -1,7 +1,7 @@
 /// jSql provides a wrapper around several common SQL implementations. Via an (almost) single set of syntax.
 /// 
-/// How this works is by using a core base syntax, which is based off mysql/sqlite. And writing and intermidiate
-/// parser for each SQL implementation. To work around its vendor specific issue, to run its respective commands.
+/// How this works is by using a core base syntax, which is based off mysql/sqlite. And writing an intermidiary
+/// parser for each SQL implementation. To work around its vendor specific issue, and run its respective commands.
 /// 
 /// Currently Supported SQL Databases
 /// + MySQL

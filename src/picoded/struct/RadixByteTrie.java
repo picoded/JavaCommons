282a71bfd48c7266617dcdf65f983942c306c8c2
Eugene Cheah <eugene@socialoctet.com>
2015-03-02 17:54:55 +0000

    Experimental Trie structures

---
+++ src/picoded/struct/RadixByteTrie.java
@@ -0,0 +1,40 @@
+package picoded.struct;
+
+import java.util.Map;
+
+/// Implements a byte array RadixTrie, also known as patricia trie, or prefix tree
+///
+/// See: http://en.wikipedia.org/wiki/Radix_tree
+///
+/// ### Example Usage
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.java}
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///
+/// ### LineCache optimisation notes.
+/// The following aims to keep its this class object (and its decendents), and its internal vars,
+/// within CPU line cache size (Assumed 64 bytes)
+///
+/// + Java Object Overhead  = 12
+/// + Inherited from ByteKeyArray = 29 - 12 (Java Object Overhead does not add up)
+/// + parentNode   = 8
+/// + storedValue  = 8
+/// + prefixArray  = 8
+/// + prefixLength = 1
+///
+/// *Total = 54 bytes*
+///
+public class RadixByteTrie<V> /* implements Map<K,V> */ {
+	
+	/// Parent node above this tree node.
+	/// if this is null, node is considered root
+	private RadixByteTrie<V> parentNode = null;
+	
+	/// Stored value of the node, if any
+	private V storeValue = null;
+	
+	///Prefix
+	private byte[] prefixArray = null;
+
+}
---
+++ src/picoded/struct/RadixTrie.java
@@ -0,0 +1,17 @@
+package picoded.struct;
+
+import java.util.Map;
+
+/// Implements a RadixTrie, also known as patricia trie, or prefix tree
+///
+/// See: http://en.wikipedia.org/wiki/Radix_tree
+///
+/// ### Example Usage
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.java}
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///
+public class RadixTrie<K,V> /* implements Map<K,V> */ {
+	
+}

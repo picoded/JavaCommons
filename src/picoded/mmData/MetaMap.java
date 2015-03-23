5c35df33da63cbe1042f3d0efe9f567b7b2346df
Eugene Cheah <eugene@picoded.com>
2015-03-24 00:00:55 +0800

    MetaMap node prototype

---
+++ src/picoded/mmData/MetaMap.java
@@ -0,0 +1,18 @@
+package picoded.mmData;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import picoded.jSql.*;
+import picoded.jCache.*;
+import java.util.logging.*;
+import org.apache.commons.lang3.StringUtils;
+
+public class MetaMap {
+	
+	/// The constructor, based on the JStack values
+	public MetaMap(JCache[] jCacheArr, JSql[] jSqlArr, String mapName) {
+		
+	}
+	
+}

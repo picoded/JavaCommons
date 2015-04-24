ef0128180235b38c69ecfca0f1f2de1ffc382aa6
Eugene Cheah <eugeneqin@gmail.com>
2015-04-24 17:45:28 +0800

    GUID conversion addons

--- src/picoded/conv/GUID.java
+++ src/picoded/conv/GUID.java
@@ -5,6 +5,8 @@ import org.apache.commons.codec.binary.Base64;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
+import picoded.conv.Base58;
+
 /// Provides several core GUID functionalities
 ///
 /// ------------------------------------------------
@@ -14,20 +16,43 @@ import java.util.UUID;
 public class GUID {
 	
 	/// Proxies UUID.randomUUID();
+	///
+	/// @returns UUID representing the GUID
 	public static UUID randomUUID() {
 		return UUID.randomUUID();
 	}
 	
-	/// Returns a long[2] array representing a guid
+	//---------------------------------------------------------------------------------------------
+	// Generates various formats of a GUID value, either randomly or via a valid UUID
+	//---------------------------------------------------------------------------------------------
+	
+	/// RANDOMLY Returns a long[2] array representing a guid
+	///
+	/// @returns long[2], representing the most, and least significant bits
 	public static long[] longPair() {
-		UUID uuid = UUID.randomUUID();
+		return longPair(randomUUID());
+	}
+	
+	/// Returns a long[2] array representing a guid
+	///
+	/// @param   uuid unique guid value
+	/// @returns long[2], representing the most, and least significant bits
+	public static long[] longPair(UUID uuid) {
 		return new long[] { uuid.getMostSignificantBits(), uuid.getLeastSignificantBits() };
 	}
 	
-	// Returns a byte[16] array representing a guid
+	/// RANDOMLY Returns a byte[16] array representing a guid
+	///
+	/// @returns byte[16] array, representing the most, and least significant bits
 	public static byte[] byteArray() {
-		UUID uuid = UUID.randomUUID();
-		
+		return byteArray(randomUUID());
+	}
+	
+	/// Returns a byte[16] array representing a guid
+	///
+	/// @param  uuid unique guid value
+	/// @returns byte[16] array, representing the most, and least significant bits
+	public static byte[] byteArray(UUID uuid) {
 		// Converts the uuid into the byteArray format
 		// http://stackoverflow.com/questions/2983065/guid-to-bytearray
 		ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
@@ -36,11 +61,20 @@ public class GUID {
 		return bb.array();
 	}
 	
-	/// Returns a 22 character base64 GUID string
-	/// Refer to: http://stackoverflow.com/questions/607651/how-many-characters-are-there-in-a-guid
-	/// & http://blog.codinghorror.com/equipping-our-ascii-armor/
+	/// RANDOMLY Returns a 22 character base64 GUID string
+	///
+	/// @param  uuid unique guid value
+	/// @returns long array of length 2, representing the most, and least significant bits
 	public static String base64() {
-		byte[] uuidArr = byteArray();
+		return base64(randomUUID());
+	}
+	
+	/// Returns a 22 character base64 GUID string
+	///
+	/// @param  uuid unique guid value
+	/// @returns string of 22 characters representing the GUID
+	public static String base64(UUID uuid) {
+		byte[] uuidArr = byteArray(uuid);
 		
 		// Convert a byte array to base64 string : for safe storing of GUID in jSql
 		String s = new Base64().encodeAsString(uuidArr).replaceAll("=", "");
@@ -48,6 +82,69 @@ public class GUID {
 			throw new RuntimeException("GUID generation exception, invalid length of " + s.length() + " (" + s + ")");
 		}
 		return s.substring(0, 22); //remove uneeded
+	}
+	
+	/// RANDOMLY Returns a 22 character base58 GUID string
+	///
+	/// @param   uuid unique guid value
+	/// @returns string of 22 characters representing the GUID
+	public static String base58() {
+		return base58(randomUUID());
+	}
+	
+	/// Returns a 22 character base58 GUID string
+	///
+	/// @param  uuid unique guid value
+	/// @returns string of 22 characters representing the GUID
+	public static String base58(UUID uuid) {
+		byte[] uuidArr = byteArray(uuid);
+		return Base58.obj.encode(uuidArr);
+	}
+	
+	//---------------------------------------------------------------------------------------------
+	// Converts the various fromat into a UUID object
+	//---------------------------------------------------------------------------------------------
+	
+	/// Returns a UUID using a long[2] most and least signficant bits pair
+	///
+	/// @param   most significant bits pair
+	/// @param   least significant bits pair
+	/// @returns A UUID object
+	public static UUID fromLongPair(long mostSignificant, long leastSignificant) {
+		return new UUID(mostSignificant, leastSignificant);
+	}
+	
+	/// Returns a UUID using a long[2] most and least signficant bits pair
+	///
+	/// @param   long[2] pair with the most and least significant bits pair
+	/// @returns A UUID object
+	public static UUID fromLongPair(long[] longPair) {
+		return new UUID(longPair[0], longPair[1]);
+	}
+	
+	/// Returns a UUID using a byte[16]
+	///
+	/// @param   byte[16] value of the UUID
+	/// @returns A UUID object
+	public static UUID fromByteArray(byte[] byteArray) {
+		ByteBuffer bb = ByteBuffer.wrap(byteArray);
+		return fromLongPair(bb.getLong(0), bb.getLong(8));
+	}
+	
+	/// Returns a UUID using a base64 GUID string
+	///
+	/// @param   base64 string to convert from
+	/// @returns A UUID object
+	public static UUID fromBase64(String base64str) {
+		return fromByteArray(Base64.decodeBase64(base64str));
+	}
+	
+	/// Returns a UUID using a base58 GUID string
+	///
+	/// @param   base64 string to convert from
+	/// @returns A UUID object
+	public static UUID fromBase58(String base58str) {
+		return fromByteArray(Base58.obj.decode(base58str));
 	}
 	
 }

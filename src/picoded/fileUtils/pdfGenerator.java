2d01be5cc62b384362acf27cf2b0bbe99516f4da
Rashmi Singh <rashmis19oct@gmail.com>
2015-05-05 05:06:29 +0000

    Class change to fileUtils.PDFGenerator its referring to the test images, used to generate the file

---
+++ src/picoded/fileUtils/pdfGenerator.java
@@ -0,0 +1,98 @@
+package picoded.fileUtils;
+
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/// pdfGenerator is a utility class to covert either a HTML string or a HTML file to a PDF file
+///
+/// ### Example Usage
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.java}
+///
+/// // covert a HTML file to a pdf file
+///
+/// String pdfOutputFile = //test-files/temp/fileUtils//pdfFile.pdf
+/// String htmlInputFile = //test-files/fileUtils/pdfGenerator/pdf-generator-html.html
+/// pdfGenerator.generatePDFfromHTML(pdfOutputFile, htmlInputFile);
+///
+/// // covert a HTML string to a pdf file
+///
+/// String pdfOutputFile = //test-files/temp/fileUtils//pdfFile.pdf
+/// String htmlString = "<table><tr><th>Cell A</th></tr><tr><td>Cell Data</td></tr></table>"
+/// pdfGenerator.generatePDFfromRawHTML(pdfOutputFile, htmlString);
+///
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///
+public class pdfGenerator {
+	
+	/// Generates a pdf file given the HTML file path
+	///
+	/// @param   pdfFile         pdf file path string
+	/// @param   htmlFilePath    HTML file path string
+	///
+	/// @returns  true if the HTML file is converted and saved in a pdf file
+	public static boolean generatePDFfromHTML(String pdfFile, String htmlFilePath) {
+		OutputStream outputStream = null;
+		try {
+			String url2 = new File(htmlFilePath).toURI().toString();
+			outputStream = new FileOutputStream(pdfFile);
+			ITextRenderer renderer = new ITextRenderer();
+			//renderer.setDocument(new File(url2).toURI().toString());
+			renderer.setDocument(new File(htmlFilePath));
+			renderer.writeNextDocument();
+			renderer.layout();
+			
+			// generate pdf
+			renderer.createPDF(outputStream);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return true;
+	}
+	
+	/// Generates a pdf file given the RAW html string
+	///
+	/// @param   rawHtml          raw HTML string
+	/// @param   outputpdfpath    pdf file path string
+	///
+	/// @returns  true if the HTML raw string is converted and saved in a pdf file.
+	public static boolean generatePDFfromRawHTML(String rawHtml, String outputpdfpath) {
+		OutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(outputpdfpath);
+			ITextRenderer renderer = new ITextRenderer();
+			renderer.setDocumentFromString(rawHtml);
+			renderer.layout();
+			
+			// generate pdf
+			renderer.createPDF(outputStream);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ src/picodedTests/fileUtils/pdfGenerator_test.java
@@ -0,0 +1,71 @@
+package picodedTests.fileUtils;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.*;
+
+import picoded.fileUtils.pdfGenerator;
+
+///
+/// Test Case for picoded.util.pdfGenerator
+///
+public class pdfGenerator_test {
+	private String outputPdfFile = null;
+	private String inputHTMLFile = null;
+	
+	@Before
+	public void setUp() {
+		inputHTMLFile = "./test-files/fileUtils/pdfGenerator/pdf-generator-html.html";
+		outputPdfFile = "./test-files/temp/fileUtils/test.pdf";
+	}
+	
+	@After
+	public void tearDown() {
+	}
+	
+	///
+	/// Test HTML file conversion to a PDF file
+	///
+	@Test
+	public void generatePDFfromHTML() throws FileNotFoundException, IOException {
+		assertTrue(pdfGenerator.generatePDFfromHTML(outputPdfFile, inputHTMLFile));
+	}
+	
+	///
+	/// Test HTML raw string conversion to a PDF file
+	///
+	@Test
+	public void generatePDFfromRawHTML() throws FileNotFoundException, IOException {
+		assertTrue(pdfGenerator.generatePDFfromRawHTML(readHTMLFile(inputHTMLFile), outputPdfFile));
+	}
+	
+	/// Reads a HTML file in a string and returns
+	private String readHTMLFile(String inputHTMLFile) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(inputHTMLFile));
+		StringBuilder stringBuilder = new StringBuilder();
+		String line = reader.readLine();
+		while (line != null) {
+			stringBuilder.append(line);
+			stringBuilder.append("\n");
+			line = reader.readLine();
+		}
+		reader.close();
+		return stringBuilder.toString();
+	}
+}

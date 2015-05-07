1471daec61f6e540e6d6935fa65aa956b263eedb
Eugene Cheah <eugeneqin@gmail.com>
2015-05-07 15:21:06 +0800

    Added md5 / shall hashing support to baseX module

---
+++ src/picoded/jStack/MetaTable.java
@@ -0,0 +1,25 @@
+package picoded.jStack;
+
+/// Java imports
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.*;
+
+/// Picoded imports
+import picoded.jSql.*;
+
+/// hazelcast
+import com.hazelcast.core.*;
+import com.hazelcast.config.*;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+///
+public class MetaTable {
+	
+}
---
+++ src/picoded/jStack/package-info.java
@@ -0,0 +1,5 @@
+/// jStack provides various common data storage format, that utalizes a combination of
+/// jCache, and jSql instances implementation.
+///
+/// The design principle is based on the prototyping experience for mmObjDB, and the original servlet-commons implementation of metaTables.
+package picoded.jStack;

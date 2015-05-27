e0c78eb2ab8fd3027d608fecacedcdd8012e9ba9
Dheeraj Singh <blstechds@gmail.com>
2015-05-27 11:34:36 +0000

    ServletLogging

--- src/picoded/util/ServletLogging.java
+++ src/picoded/util/ServletLogging.java
@@ -1,6 +1,14 @@
 package picoded.util;
 
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.util.UUID;
+
+import org.apache.commons.codec.binary.Base64;
+
 import picoded.jSql.*;
+import picoded.conv.Base58;
 
 /// SerlvetLogging, is a utility class meant to facilitate the logging of server sideded application events, and errors
 /// This is meant to push the logging into a central SQL database, and fallbacks onto its local SQLite file, in event the
@@ -26,131 +34,261 @@ import picoded.jSql.*;
 ///   - you may need to settle issue #36 for this to work
 ///
 public class ServletLogging {
-	
-	public ServletLogging(JSql sqlite, JSql db) {
-		
-	}
-	
-	public ServletLogging(String sqlitePath, JSql db) {
-		
-	}
-	
-	public int longIndexed = 04;
-	public int stringIndexed = 04;
-	
-	public int longNotIndexed = 04;
-	public int stringNotIndexed = 04;
-	
-	public int exceptionRootTraceIndexed = 02;
-	public int exceptionThrownTraceIndexed = 02;
-	
-	/// Performs the needed table setup, required for the class. Do note that this includes the setup of logging formats and its respective tables
-	/// Note that this table exists on both the sqlite, and the actual DB, all tables keep their data locally on sqlite, except logTable,
-	/// which pushes to the central DB, and clears locally.
-	///
-	/// # PREFIX_logFormat
-	/// This table represents the raw table formats, used by the logging framework, and has the following collumns
-	///
-	/// - hash
-	/// - format (indexed, unique)
-	/// - name (indexed, unique)
-	///
-	/// # PREFIX_logStrHashes
-	/// Stores strings larger then certain size (21?) and hash them, this is so that duplicate strings can be found and reused
-	///
-	/// - hash (indexed)
-	/// - sVal (indexed, unique TEXT string)
-	///
-	/// # PREFIX_logTable
-	/// The actual log storage, note that all string values that are larger then a defined size (21?) is stored in PREFIX_logStrHashes instead,
-	/// where its hash value is used. Note that the process to check for existing hashes, is done locally on the sqlite, before the central DB.
-	///
-	/// - instID  (indexed)        // instance ID, used to trace the logging source
-	/// - reqsID  (indexed)        // request ID, used to trace the logging source
-	/// - creTime (indexed)        // created unix timestamp
-	/// - fmtHash (indexed)        // format string hash, see PREFIX_logStrHashes
-	/// - logType (indexed)        // log data type
-	/// - expHash (indexed)        // exception hash, see PREFIX_expHash
-	/// - offSync (indexed)        // offline sync boolean indicator
-	/// - reqID   (indexed)        // request ID, generated on each request session
-	/// - l01-XX  (indexed)        // long values used in the format, %IP4 is converted to long values
-	/// - s01-XX  (indexed)        // varchar string value storage, varchar(22)
-	/// - lXX-YY  (not indexed)    // long values used in the format that is NOT indexed
-	/// - sXX-YY  (not indexed)    // varchar string value storage, varchar(22)
-	///
-	/// # PREFIX_excStrHash
-	/// Contains the hash values of the stack traces. This function similarly to logStrHashes
-	///
-	/// - hash (indexed)
-	/// - sVal (indexed, unique TEXT string)
-	///
-	/// # PREFIX_exception
-	/// Table consisting of exception records, split in according to stack trace. The idea here is to prevent huge exception stack traces
-	/// from flooding the database storage system, when they are essentially, the same messages.
-	///
-	/// - expHash    (indexed)     // Full exception message hash
-	/// - reqsID     (indexed)     // request ID, used to trace the logging source
-	/// - creTime    (indexed)     // Exception created timestamp
-	/// - instID     (indexed)     // instance ID, used to trace the hashing source
-	///
-	/// (note the stack messages is filled in in the following order)
-	/// - excRoot    (indexed)     // Exception root cause message
-	/// - excTrace   (indexed)     // Exception thrown by message
-	/// - excR01-XX  (indexed)     // Exception after root cause tracing messages
-	/// - excT01-XX  (indexed)     // Exception thrown by tracing messages
-	/// - excMid     (not indexed) // The remaining exception message in a JSON array
-	///
-	/// - stkRoot    (indexed)     // Stack tracing corresponding to the exception message
-	/// - stkTrace   (indexed)     // Stack tracing corresponding to the exception message
-	/// - stkR01-XX  (indexed)     // Stack tracing corresponding to the exception message
-	/// - stkT01-XX  (indexed)     // Stack tracing corresponding to the exception message
-	/// - stkMid     (not indexed) // Stack tracing corresponding to the exception message, in a JSON array
-	///
-	/// The following is sqlite only, used to cache configs, such as instanceID
-	///
-	/// # PREFIX_config
-	/// - key (unique, indexed)
-	/// - sVal
-	///
-	public ServletLogging tableSetup() {
-		return this;
-	}
-	
-	/// Returns the instance ID stored inside the SQLite DB, if it does not exists, generate one
-	public String instanceID() {
-		return null;
-	}
-	
-	/// Validate if the instance ID if it belongs to the current physical / virtual server. If it fails, it reissues the ID. Used on servlet startup
-	public String validateInstanceID() {
-		return null;
-	}
-	
-	/// Returns the current request ID
-	public String requestID() {
-		return null;
-	}
-	
-	/// Reissue a new requestID, used at start of servlet call
-	public String reissueRequestID() {
-		return null;
-	}
-	
-	/// Add the format to the system
-	public String addFormat(String formatName, String formatString) {
-		return null;
-	}
-	
-	/// Get / Has / Remove format
-	
-	/// Performs a logging with a format name and argument
-	public void logWithFormat(String formatName, Object[] args) {
-		
-	}
-	
-	/// Performs a logging with a formant name, argument, and attached exception
-	public void logWithFormat(String formatName, Object[] args, Exception e) {
-		
-	}
-	
+
+   /// Object byte space default as 260
+   protected int objColumnLength = 260;
+
+   /// Key byte space default as 260
+   protected int keyColumnLength = 260;
+
+   /// Value byte space default as 4000
+   protected int valColumnLength = 4000;
+
+   protected JSql sqliteObj;
+   protected JSql jSqlObj;
+
+   protected String requestId;
+
+   public ServletLogging(JSql sqlite, JSql db) {
+      this.sqliteObj = sqlite;
+      this.jSqlObj = db;
+   }
+
+   public ServletLogging(String sqlitePath, JSql db) {
+      this.sqliteObj = JSql.sqlite(sqlitePath);
+      this.jSqlObj = db;
+   }
+
+   public int longIndexed = 04;
+   public int stringIndexed = 04;
+
+   public int longNotIndexed = 04;
+   public int stringNotIndexed = 04;
+
+   public int exceptionRootTraceIndexed = 02;
+   public int exceptionThrownTraceIndexed = 02;
+
+   // / Performs the needed table setup, required for the class. Do note that
+   // this includes the setup of logging formats and its respective tables
+   // / Note that this table exists on both the sqlite, and the actual DB, all
+   // tables keep their data locally on sqlite, except logTable,
+   // / which pushes to the central DB, and clears locally.
+   // /
+   // / # PREFIX_logFormat
+   // / This table represents the raw table formats, used by the logging
+   // framework, and has the following columns
+   // /
+   // / - hash
+   // / - format (indexed, unique)
+   // / - name (indexed, unique)
+   // /
+   // / # PREFIX_logStrHashes
+   // / Stores strings larger then certain size (21?) and hash them, this is so
+   // that duplicate strings can be found and reused
+   // /
+   // / - hash (indexed)
+   // / - sVal (indexed, unique TEXT string)
+   // /
+   // / # PREFIX_logTable
+   // / The actual log storage, note that all string values that are larger
+   // then a defined size (21?) is stored in PREFIX_logStrHashes instead,
+   // / where its hash value is used. Note that the process to check for
+   // existing hashes, is done locally on the sqlite, before the central DB.
+   // /
+   // / - instID (indexed) // instance ID, used to trace the logging source
+   // / - reqsID (indexed) // request ID, used to trace the logging source
+   // / - creTime (indexed) // created unix timestamp
+   // / - fmtHash (indexed) // format string hash, see PREFIX_logStrHashes
+   // / - logType (indexed) // log data type
+   // / - expHash (indexed) // exception hash, see PREFIX_expHash
+   // / - offSync (indexed) // offline sync boolean indicator
+   // / - reqID (indexed) // request ID, generated on each request session
+   // / - l01-XX (indexed) // long values used in the format, %IP4 is converted
+   // to long values
+   // / - s01-XX (indexed) // varchar string value storage, varchar(22)
+   // / - lXX-YY (not indexed) // long values used in the format that is NOT
+   // indexed
+   // / - sXX-YY (not indexed) // varchar string value storage, varchar(22)
+   // /
+   // / # PREFIX_excStrHash
+   // / Contains the hash values of the stack traces. This function similarly
+   // to logStrHashes
+   // /
+   // / - hash (indexed)
+   // / - sVal (indexed, unique TEXT string)
+   // /
+   // / # PREFIX_exception
+   // / Table consisting of exception records, split in according to stack
+   // trace. The idea here is to prevent huge exception stack traces
+   // / from flooding the database storage system, when they are essentially,
+   // the same messages.
+   // /
+   // / - expHash (indexed) // Full exception message hash
+   // / - reqsID (indexed) // request ID, used to trace the logging source
+   // / - creTime (indexed) // Exception created timestamp
+   // / - instID (indexed) // instance ID, used to trace the hashing source
+   // /
+   // / (note the stack messages is filled in in the following order)
+   // / - excRoot (indexed) // Exception root cause message
+   // / - excTrace (indexed) // Exception thrown by message
+   // / - excR01-XX (indexed) // Exception after root cause tracing messages
+   // / - excT01-XX (indexed) // Exception thrown by tracing messages
+   // / - excMid (not indexed) // The remaining exception message in a JSON
+   // array
+   // /
+   // / - stkRoot (indexed) // Stack tracing corresponding to the exception
+   // message
+   // / - stkTrace (indexed) // Stack tracing corresponding to the exception
+   // message
+   // / - stkR01-XX (indexed) // Stack tracing corresponding to the exception
+   // message
+   // / - stkT01-XX (indexed) // Stack tracing corresponding to the exception
+   // message
+   // / - stkMid (not indexed) // Stack tracing corresponding to the exception
+   // message, in a JSON array
+   // /
+   // / The following is sqlite only, used to cache configs, such as instanceID
+   // /
+   // / # PREFIX_config
+   // / - key (unique, indexed)
+   // / - sVal
+   // /
+   public ServletLogging tableSetup() throws JSqlException {
+      // table setup for sqlite
+      tableSetup(sqliteObj);
+
+      // table setup for db
+      tableSetup(jSqlObj);
+
+      return this;
+   }
+
+   private void tableSetup(JSql jSqlObj) throws JSqlException {
+      // / config table
+      if (jSqlObj.sqlType == JSqlType.sqlite) {
+         jSqlObj.execute("CREATE TABLE IF NOT EXISTS `config` ( "
+                         + "key VARCHAR(" + objColumnLength + "), sVal TEXT, PRIMARY KEY (key) );");
+      }
+
+      // / logFormat table
+      jSqlObj.execute("CREATE TABLE IF NOT EXISTS `logFormat` ( "
+                      + "hash VARCHAR(" + objColumnLength + "), "
+                      + "format VARCHAR(" + keyColumnLength + "), "
+                      + "name VARCHAR(" + keyColumnLength + "), "
+                      + "PRIMARY KEY (hash) );");
+
+      // / logStrHashes table
+      jSqlObj.execute("CREATE TABLE IF NOT EXISTS `logStrHashes` ( "
+                      + "hash VARCHAR(" + objColumnLength + "), sVal TEXT, PRIMARY KEY (hash) );");
+
+      // / excStrHash table
+      jSqlObj.execute("CREATE TABLE IF NOT EXISTS `excStrHash` ( "
+                      + "hash VARCHAR(" + objColumnLength + "), sVal TEXT, PRIMARY KEY (hash) );");
+
+      // / exception table
+      jSqlObj.execute("CREATE TABLE IF NOT EXISTS `exception` ( "
+                      + "expHash VARCHAR(" + objColumnLength + "), "
+                      + "reqsID VARCHAR(" + objColumnLength + "), "
+                      + "creTime BIGINT, instID VARCHAR(60), "
+                      + "excRoot VARCHAR(" + valColumnLength + "), "
+                      + "excTrace VARCHAR(" + valColumnLength + "), "
+                      + "excR01-XX VARCHAR(" + valColumnLength + "), "
+                      + "excT01-XX VARCHAR(" + valColumnLength + "), "
+                      + "excMid VARCHAR(" + valColumnLength + "),"
+                      + "stkRoot VARCHAR(" + valColumnLength + "), "
+                      + "stkTrace VARCHAR(" + valColumnLength + "), "
+                      + "stkR01-XX VARCHAR(" + valColumnLength + "), "
+                      + "stkT01-XX VARCHAR(" + valColumnLength + "), "
+                      + "stkMid VARCHAR(" + valColumnLength + "),"
+                      + "PRIMARY KEY (expHash) );");
+
+      // / logTable table
+      jSqlObj.execute("CREATE TABLE IF NOT EXISTS `logTable` ( "
+                      + "instID VARCHAR(" + objColumnLength + "), "
+                      + "reqsID VARCHAR(" + objColumnLength + "), "
+                      + "creTime BIGINT, "
+                      + "fmtHash VARCHAR(" + objColumnLength + "), "
+                      + "logType VARCHAR(" + valColumnLength + "), "
+                      + "expHash VARCHAR(" + objColumnLength + "), "
+                      + "offSync BIT, "
+                      + "reqID VARCHAR(" + objColumnLength + "), "
+                      + "l01-XX VARCHAR(" + valColumnLength + "), "
+                      + "s01-XX VARCHAR(" + valColumnLength + "), "
+                      + "lXX-YY VARCHAR(" + valColumnLength + "), "
+                      + "sXX-YY VARCHAR(" + valColumnLength + ") )");
+   }
+
+   // / Returns the instance ID stored inside the SQLite DB, if it does not
+   // exists, generate one
+   public String instanceID() throws JSqlException {
+      String val = null;
+      JSqlResult r = sqliteObj.executeQuery("SELECT sVal FROM `config` WHERE key='instanceid'");
+      if(r.fetchAllRows() > 0){
+         val = (String)r.readRowCol(0,"sVal");
+      }
+      return val;
+   }
+
+   // / Validate if the instance ID if it belongs to the current physical /
+   // virtual server. If it fails, it reissues the ID. Used on servlet startup
+   public String validateInstanceID() {
+      return null;
+   }
+
+   // / Returns the current request ID
+   public String requestID() throws JSqlException {
+      String requestId = null;
+      JSqlResult r = jSqlObj.executeQuery("SELECT reqID FROM `logTable` WHERE instID = " + instanceID());
+      if(r.fetchAllRows() > 0){
+         requestId = (String)r.readRowCol(0, "reqID");
+      }
+      return requestId;
+   }
+
+   // / Reissue a new requestID, used at start of servlet call
+   public String reissueRequestID() throws Exception {
+      return generateHash();
+   }
+
+   // / Add the format to the system
+   public String addFormat(String formatName, String formatString) throws Exception {
+      String fmtHash = generateHash();
+      jSqlObj.execute( "INSERT INTO `logFormat` (hash, format, name) VALUES (?, ?, ?)", fmtHash, formatString, formatName );
+      return fmtHash;
+   }
+
+   // / Get / Has / Remove format
+
+   // / Performs a logging with a format name and argument
+   public void logWithFormat(String formatName, Object[] args) throws JSqlException {
+      // jSqlObj.execute( "INSERT INTO `logStrHashes` (hash, sVal) VALUES (?, ?)", "expHash", generateHash() );
+
+      jSqlObj.execute( "INSERT INTO `logTable`"
+                      + " (instID, reqsID, creTime, fmtHash, logType, expHash, offSync, reqID, l01-XX, s01-XX, lXX-YY, sXX-YY) "
+                      + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", args );
+   }
+
+   // / Performs a logging with a formant name, argument, and attached
+   // exception
+   public void logWithFormat(String formatName, Object[] args, Exception e) throws JSqlException {
+      // jSqlObj.execute( "INSERT INTO `excStrHash` (hash, sVal) VALUES (?, ?)", "expHash", generateHash() );
+
+      jSqlObj.execute( "INSERT INTO `logTable` "
+                      + "(instID, reqsID, creTime, fmtHash, logType, expHash, offSync, reqID, l01-XX, s01-XX, lXX-YY, sXX-YY) "
+                      + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", args );
+   }
+
+   // / Returns a 22 character string Base58 encoding of MD5 string
+   private String generateHash() throws Exception {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      byte[] messageDigest = md.digest(UUID.randomUUID().toString()
+                                       .getBytes());
+      String s = new Base58().encode(messageDigest);
+      if (s.length() < 22) {
+         throw new RuntimeException(
+            "GUID generation exception, invalid length of "
+            + s.length() + " (" + s + ")");
+      }
+      return s.substring(0, 22); // remove unneeded
+   }
 }

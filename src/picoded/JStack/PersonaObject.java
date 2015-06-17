eb7c68c82d31e05ee66c4960311344bf659df5ae
Eugene Cheah <eugeneqin@gmail.com>
2015-06-17 19:43:55 +0800

    Basic underlying structure setup for PersonaTable class

---
+++ src/picoded/JStack/PersonaObject.java
@@ -0,0 +1,38 @@
+package picoded.JStack;
+
+/// Java imports
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+/// Picoded imports
+import picoded.conv.GUID;
+import picoded.JSql.*;
+import picoded.JCache.*;
+import picoded.struct.CaseInsensitiveHashMap;
+import picoded.struct.UnsupportedDefaultMap;
+import picoded.struct.GenericConvertMap;
+
+/// hazelcast
+import com.hazelcast.core.*;
+import com.hazelcast.config.*;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class PersonaObject implements GenericConvertMap<String, Object>  {
+
+	public PersonaObject() {
+
+	}
+
+}
---
+++ src/picoded/JStack/PersonaTable.java
@@ -0,0 +1,131 @@
+package picoded.JStack;
+
+/// Java imports
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+/// Picoded imports
+import picoded.conv.GUID;
+import picoded.JSql.*;
+import picoded.JCache.*;
+import picoded.struct.CaseInsensitiveHashMap;
+import picoded.struct.UnsupportedDefaultMap;
+import picoded.struct.GenericConvertMap;
+
+/// hazelcast
+import com.hazelcast.core.*;
+import com.hazelcast.config.*;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+///
+/// The persona class is considered a hybrid class of both the user, group management class.
+/// Where both users, and groups are considered the "same". Hence their distinction is normally detirmined
+/// by one of the meta field values.
+///
+/// The intention here, is to facilitate complex hierachy creations rapidly. Especially against changing specs.
+///
+public class PersonaTable extends JStackData implements UnsupportedDefaultMap<String, PersonaObject>  {
+
+	///
+	/// Constructor setup
+	///--------------------------------------------------------------------------
+
+	/// Setup the metatable with the default table name
+	public PersonaTable(JStack inStack) {
+		super( inStack );
+		tableName = "PersonaTable";
+
+		internalConstructor(inStack);
+	}
+
+	/// Setup the metatable with the given stack
+	public PersonaTable(JStack inStack, String inTableName) {
+		super( inStack, inTableName );
+
+		internalConstructor(inStack);
+	}
+
+	/// Internal constructor that setsup the underlying MetaTable / KeyValuePair
+	public void internalConstructor(JStack inStack) {
+		personaID = new KeyValueMap(inStack, tableName+PERSONA_ID);
+		personaSessions = new KeyValueMap(inStack, tableName+PERSONA_SESSIONS);
+		personaMeta = new MetaTable(inStack, tableName+PERSONA_META);
+		personaChild = new MetaTable(inStack, tableName+PERSONA_CHILD);
+		personaChildMeta = new MetaTable(inStack, tableName+PERSONA_CHILDMETA);
+	}
+
+	///
+	/// Table suffixes for the variosu sub tables
+	///--------------------------------------------------------------------------
+
+	/// The persona self ID's
+	protected static String PERSONA_ID = "_ID";
+
+	/// The login sessions used for authentication
+	protected static String PERSONA_SESSIONS = "_LS";
+
+	/// The persona self meta values
+	protected static String PERSONA_META = "_SM";
+
+	/// The child nodes mapping, from self
+	protected static String PERSONA_CHILD = "_SC";
+
+	/// The child persona meta values
+	protected static String PERSONA_CHILDMETA = "_CM";
+
+	///
+	/// Underlying data structures
+	///--------------------------------------------------------------------------
+
+	/// @TODO better docs
+	protected KeyValueMap personaID = null;
+
+	/// @TODO better docs
+	protected KeyValueMap personaSessions = null;
+
+	/// @TODO better docs
+	protected MetaTable personaMeta = null;
+
+	/// @TODO better docs
+	protected MetaTable personaChild = null;
+
+	/// @TODO better docs
+	protected MetaTable personaChildMeta = null;
+
+	//
+	// JStack common setup functions
+	//--------------------------------------------------------------------------
+
+	/// Performs the full stack setup for the data object
+	public void stackSetup() throws JStackException {
+		personaID.stackSetup();
+		personaSessions.stackSetup();
+		personaMeta.stackSetup();
+		personaChild.stackSetup();
+		personaChildMeta.stackSetup();
+	}
+
+	/// Performs the full stack teardown for the data object
+	public void stackTeardown() throws JStackException {
+		personaID.stackTeardown();
+		personaSessions.stackTeardown();
+		personaMeta.stackTeardown();
+		personaChild.stackTeardown();
+		personaChildMeta.stackTeardown();
+	}
+
+	
+
+}

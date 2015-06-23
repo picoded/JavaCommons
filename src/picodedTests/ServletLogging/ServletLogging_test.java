236dd6f1eb4cd2e91b35a9157bc5299626f92bdd
thenet <thenet>
2015-06-23 05:59:26 +0000

    ServletLogging class is moved to new package picoded.ServletLogging

---
+++ src/picodedTests/ServletLogging/ServletLogging_test.java
@@ -0,0 +1,90 @@
+package picodedTests.ServletLogging;
+
+import java.util.List;
+import java.util.Map;
+
+import picoded.conv.GUID;
+import picoded.JSql.JSql;
+import picoded.JSql.JSqlException;
+
+// Target test class
+import picoded.ServletLogging.ServletLogging;
+
+// Test Case include
+import org.junit.*;
+import static org.junit.Assert.*;
+
+///
+/// Test Case for picoded.struct.CaseInsensitiveHashMap
+///
+public class ServletLogging_test {
+	
+	protected JSql JSqlObj = null;
+	protected ServletLogging slObj = null;
+
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		// one-time initialization code
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		// one-time cleanup code
+	}
+	
+	@Before
+	public void setUp() throws JSqlException {
+		JSqlObj = JSql.sqlite();
+		
+		slObj = new ServletLogging(JSqlObj, JSqlObj);
+		slObj.tableSetup();
+	}
+	
+	@After
+	public void tearDown() throws JSqlException {
+		if (JSqlObj != null) {
+			slObj.tearDown();
+			JSqlObj.dispose();
+			JSqlObj = null;
+			}catch(Exception e){
+          e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	public void validateSystemHash() throws JSqlException {
+		assertNotNull("SQL result returns as expected", slObj.validateSystemHash());
+	}
+
+	@Test
+	public void systemHash() throws JSqlException {
+		assertNotNull("SQL result returns as expected", slObj.systemHash());
+	}
+
+	@Test
+	public void addFormat() throws JSqlException {
+		String fmtHash = GUID.base58();
+		slObj.addFormat(fmtHash, "log user %s, at time %s from %s.");
+	}
+	
+	@Test
+	public void log() throws JSqlException {
+		Object[] args = {"XYZ", (int)(System.currentTimeMillis() / 1000), "SG"};
+		slObj.log("log user %s, at time %i from %s.", args);
+	}
+	
+	@Test
+	public void list() throws JSqlException {
+		int time = (int)(System.currentTimeMillis() / 1000);
+		// save log
+		Object[] args = {"brown", "dog", "over", "wall", time, time, time, time, "SG"};
+		slObj.log("The %s lazy %s jumps %s the %s, at time %i %i %i %i in %s.", args);
+
+		// fetch all logs
+		List<Map<String, Object>> list = slObj.list();
+		// System.out.println(list);
+		assertNotNull("SQL result returns as expected", list);
+	}
+	
+}

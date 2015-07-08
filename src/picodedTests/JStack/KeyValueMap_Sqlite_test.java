b451aad218a1b12a30e6a4b71bdcd37cfe71a829
thenet <thenet>
2015-07-08 15:10:53 +0000

    test case added for expired key testing which should returns null after expired

--- src/picodedTests/JStack/KeyValueMap_Sqlite_test.java
+++ src/picodedTests/JStack/KeyValueMap_Sqlite_test.java
@@ -110,6 +110,22 @@ public class KeyValueMap_Sqlite_test extends JStackData_testBase_test {
 	   assertNotNull(kvObj.getLifespan("yes"));
 	}
 	
+	@Test
+	public void testColumnExpiration() throws Exception {
+	   //set column expiration time to current time + 30 secs.
+	   long expirationTime = currentSystemTime_seconds() + 30;
+	   kvObj.putWithExpiry("yes", "no", expirationTime);
+
+	   //before the expiration time key will not be null.
+	   assertNotNull(kvObj.get("yes"));
+
+	   //sleep the execution for 31 secs so that key gets expired.
+	   Thread.sleep(31000);
+
+	   //key should be null after expiration time.
+	   assertEquals( null, kvObj.get("yes"));
+	}
+
 	private long currentSystemTime_seconds() {
 		return (System.currentTimeMillis() / 1000L);
 	}

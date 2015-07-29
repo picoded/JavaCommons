be1ca31125fe621a9d3facd05be7374c4c44711c
Dheeraj <blstechds@gmail.com>
2015-07-29 12:27:40 +0000

    1) Last commit changes of MetaTable_Sqlite_test.java reverted, 2) Code formatted

--- src/picoded/conv/RegexUtils.java
+++ src/picoded/conv/RegexUtils.java
@@ -1,15 +1,15 @@
 package picoded.conv;
 
-public class RegexUtils{
+public class RegexUtils {
 	public static String removeAllWhiteSpace_regexString = "\\s";
 	
-	public static String removeAllWhiteSpace(String input){
+	public static String removeAllWhiteSpace(String input) {
 		return input.replaceAll(removeAllWhiteSpace_regexString, "");
 	}
-
+	
 	public static String removeAllNonAlphaNumeric_regexString = "[^a-zA-Z0-9\\\\s]";
 	
-	public static String removeAllNonAlphaNumeric(String input){
+	public static String removeAllNonAlphaNumeric(String input) {
 		return input.replaceAll(removeAllNonAlphaNumeric_regexString, "");
 	}
 }

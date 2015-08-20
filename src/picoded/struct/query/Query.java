60ba143fd7f5045edc27a08a1b73aa7ace7b0cff
Eugene Cheah <eugeneqin@gmail.com>
2015-08-20 16:12:38 +0800

    Query interface extended.

--- src/picoded/struct/query/Query.java
+++ src/picoded/struct/query/Query.java
@@ -4,11 +4,13 @@ import java.util.function.*;
 import java.util.*;
 
 ///
-/// 
-/// 
-/// 
+/// Representas a query condition, that can be used as a java Predicate against a collection
 ///
 public interface Query extends Predicate<Object> {
+	
+	//
+	// Public test functions
+	//--------------------------------------------------------------------
 	
 	/// The test operator, asserts if the element matches
 	/// against its cached argument map value
@@ -28,5 +30,25 @@ public interface Query extends Predicate<Object> {
 	///
 	/// @returns  boolean indicating true / false
 	boolean test(Object t, Map<String,Object>argMap);
+	
+	//
+	// Public accessors
+	//--------------------------------------------------------------------
+	
+	/// Gets the query type
+	public QueryType type();
+	
+	//
+	// condition only accessors
+	//--------------------------------------------------------------------
+	
+	/// Gets the field name
+	public String fieldName();
+	
+	/// Gets the argument name
+	public String argumentName();
+	
+	/// Gets the default argument map
+	public Map<String,Object> defaultArgumentMap();
 	
 }

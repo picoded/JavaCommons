b66cef84bd00b0ebb6856592c37774a6ba5927b5
Dheeraj <blstechds@gmail.com>
2015-09-15 12:09:37 +0000

    test case added for start method

--- src/picodedTests/utils/StreamRedirectThread_test.java
+++ src/picodedTests/utils/StreamRedirectThread_test.java
@@ -15,33 +15,52 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 
 public class StreamRedirectThread_test {
     
     private StreamRedirectThread srt = null;
-    private InputStream input = null;
-    private OutputStream output = null;
+    
+    private File inputFile = null;
     private File outputFile = null;
 	
 	@Before
-	public void setUp() throws FileNotFoundException {
-		File outputFolder = new File("./test-files/tmp/utils/StreamRedirectThread/");
+	public void setUp() throws IOException, FileNotFoundException {
+	    // output file folder
+		File outputFolder = new File( "./test-files/tmp/utils/StreamRedirectThread/" );
+		
 		// makes the output directory tmporary folder as needed
 		outputFolder.mkdirs();
-		outputFile = new File(outputFolder, "outputFile.txt");
 		
-		input = new FileInputStream("./test-files/test-specific/utils/StreamRedirectThread/inputFile.txt");
-		output = new FileOutputStream(outputFile);
+		// output file
+		outputFile = new File( outputFolder, "outputFile.txt" );
 		
-		srt = new StreamRedirectThread(input, output);
+		// input file
+		inputFile = new File( "./test-files/test-specific/utils/StreamRedirectThread/inputFile.txt" );
+		
+		srt = new StreamRedirectThread( new FileInputStream( inputFile ), new FileOutputStream( outputFile ) );
 	}
 	
-	@Test @SuppressWarnings("unchecked")
+	@Test
 	public void runTest() throws IOException, FileNotFoundException {
+		srt.run();
+		
+		// output file should be exist
+		assertTrue( outputFile.exists() );
+		
+		// match contents of input and output files
+		assertEquals( FileUtils.readFileToString(inputFile), FileUtils.readFileToString(outputFile) );
+	}
+
+	@Test
+	public void startTest() throws IOException, FileNotFoundException {
 		srt.start();
 		
-		assertTrue(outputFile.exists());
-		assertEquals(IOUtils.toByteArray(input), IOUtils.toByteArray(new FileInputStream(outputFile)));
+		// output file should be exist
+		assertTrue( outputFile.exists() );
+		
+		// match contents of input and output files
+		assertEquals( FileUtils.readFileToString(inputFile), FileUtils.readFileToString(outputFile) );
 	}
 
 }

b0873393d0bc1019e7d14ff9243432a827385bd2
Samuel Barnard <samuel@socialoctet.com>
2015-09-18 14:12:00 +0800

    Removed processing - moved it to javascript

--- src/picoded/webTemplateEngines/FormGenerator/FormWrapperTemplates.java
+++ src/picoded/webTemplateEngines/FormGenerator/FormWrapperTemplates.java
@@ -331,11 +331,6 @@ public class FormWrapperTemplates {
 		
 		String fieldValue = node.getFieldName();
 		
-		//need to process any square brackets or fullstops if not javascript wont like it
-		fieldValue = fieldValue.replace("[", "\\\\[");
-		fieldValue = fieldValue.replace("]", "\\\\]");
-		fieldValue = fieldValue.replace(".", "\\\\.");
-		
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("onclick", "showSignatureBox('#"+fieldValue+"')"); //hardcoded function name as it will be part of the prefix, and standardised
 		

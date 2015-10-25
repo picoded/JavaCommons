f1b5f3426676bcf22011d4994cf8c4f4fdb18734
Eugene Cheah <eugeneqin@gmail.com>
2015-10-25 16:59:21 +0800

    Removed JStack interim, as JSql implementation.

--- src/picoded/JStack/JStack.java
+++ src/picoded/JStack/JStack.java
@@ -14,7 +14,7 @@ import picoded.JStack.struct.*;
 /// JCache, and JSql instances implementation.
 ///
 /// The design principle is based on the prototyping experience for mmObjDB, and the original servlet-commons implementation of metaTables.
-public class JStack extends JSqlStruct implements JStackLayer {
+public class JStack extends JStruct implements JStackLayer {
 	
 	//----------------------------------------------
 	// Readonly internal variables
@@ -65,24 +65,10 @@ public class JStack extends JSqlStruct implements JStackLayer {
 	
 	public JStack(JStackLayer inLayer) {
 		_stackLayers = new JStackLayer[] { inLayer };
-		interimJSqlOnly();
 	}
 	
 	public JStack(JStackLayer[] inStack) {
 		_stackLayers = inStack;
-		interimJSqlOnly();
-	}
-	
-	/// This is used to setup JStack as a JSQL only system.
-	protected void interimJSqlOnly() {
-		JStackLayer[] layers = stackLayers();
-		
-		JStackLayer last = layers[layers.length - 1];
-		if (last instanceof JSql) {
-			sqlObj = (JSql) last;
-		} else {
-			throw new RuntimeException("JStack currently supports a single JSql node (@TODO : Fix this)");
-		}
 	}
 	
 	//----------------------------------------------

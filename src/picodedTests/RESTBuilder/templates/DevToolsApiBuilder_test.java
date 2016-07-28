24af0ed955fcd6a8a7baf84467be7a8a5f7fba10
Rahsmi Singh <rashmis19oct@gmail.com>
2016-07-28 11:42:26 +0530

    fixed - test cases build warnings

--- src/picodedTests/RESTBuilder/templates/DevToolsApiBuilder_test.java
+++ src/picodedTests/RESTBuilder/templates/DevToolsApiBuilder_test.java
@@ -1,36 +1,30 @@
 package picodedTests.RESTBuilder.templates;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.apache.catalina.LifecycleException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import picoded.conv.*;
-import picoded.JStruct.JStruct;
-import picoded.JStruct.MetaObject;
-import picoded.JStruct.MetaTable;
-import picoded.RESTBuilder.*;
-import picoded.RESTBuilder.templates.*;
-import picoded.conv.GUID;
+import picoded.RESTBuilder.RESTBuilder;
+import picoded.RESTBuilder.templates.DevToolsApiBuilder;
+import picoded.conv.ConvertJSON;
 import picoded.servlet.BasePage;
 import picoded.servletUtils.EmbeddedServlet;
 import picoded.webUtils.RequestHttp;
 import picoded.webUtils.ResponseHttp;
 
+@SuppressWarnings("try")
 public class DevToolsApiBuilder_test {
 	protected static EmbeddedServlet tomcat = null;
 	protected static String testAddress = "http://127.0.0.1:";
@@ -105,13 +99,13 @@ public class DevToolsApiBuilder_test {
 	@Test
 	public void mapTesting() {
 		assertEquals("{}", ConvertJSON.fromMap(rbObj.namespaceTree()));
-		builder.setupRESTBuilder(rbObj, "dev.");
+		DevToolsApiBuilder.setupRESTBuilder(rbObj, "dev.");
 		assertNotEquals("{}", ConvertJSON.fromMap(rbObj.namespaceTree()));
 	}
 	
 	//@Test
 	public void api_tree_GETTest() {
-		builder.setupRESTBuilder(rbObj, builder, "/meta-test/");
+		DevToolsApiBuilder.setupRESTBuilder(rbObj, builder, "/meta-test/");
 		assertNotEquals("{}", ConvertJSON.fromMap(rbObj.namespaceTree()));
 		Map<String, String[]> paramsMap = new HashMap<String, String[]>();
 		response = RequestHttp.get(testAddress + "/api/tree/api_tree_GET", paramsMap);

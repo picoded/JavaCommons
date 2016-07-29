de1e49c0ce7b9c3827a29147b2ec7c23f6e16d39
Eugene Cheah <eugeneqin@gmail.com>
2016-07-29 12:09:36 +0800

    Experimental ES6 support

---
+++ src/picodedTests/conv/CompileES6_test.java
@@ -0,0 +1,23 @@
+package picodedTests.conv;
+
+// Target test class
+import java.util.Calendar;
+
+import picoded.conv.*;
+
+// Test Case include
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class CompileES6_test {
+	@Test
+	public void simpleConvert() {
+		
+		String es6 = "class hello { echo() { return 'hello world'; } }; class world extends hello { }";
+		String es5 = "";
+		
+		assertNotNull( es5 = CompileES6.compile(es6) );
+		assertEquals( es6, es5 );
+	}
+}

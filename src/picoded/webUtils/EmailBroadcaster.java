a60e4fa99096c0d97099a6a3af3cadea93539fa6
Samuel Barnard <samuel@socialoctet.com>
2016-08-05 18:39:15 +0800

    Trying to make java mail work with exchange server

--- src/picoded/webUtils/EmailBroadcaster.java
+++ src/picoded/webUtils/EmailBroadcaster.java
@@ -23,10 +23,10 @@ import javax.mail.internet.MimeMultipart;
 /// This is embedded inside CommonsPage
 ///
 public class EmailBroadcaster {
-	
+
 	private Session session;
 	private String fromEmail;
-	
+
 	///
 	/// Email constructor, used to setup the SMTP connection
 	///
@@ -36,10 +36,10 @@ public class EmailBroadcaster {
 	/// @Params  Sending email address
 	///
 	public EmailBroadcaster(final String smtpUrl, final String username, final String password, final String fromAddress) {
-		
-		this(smtpUrl, username, password, fromAddress, false);
+
+		this(smtpUrl, username, password, fromAddress, false, false);
 	}
-	
+
 	///
 	/// Email constructor, used to setup the SMTP over SSL connection
 	///
@@ -50,30 +50,37 @@ public class EmailBroadcaster {
 	/// @Params  Use SSL over SMTP
 	///
 	public EmailBroadcaster(final String smtpUrl, final String username, final String password,
-		final String fromAddress, boolean isSSL) {
-		
+		final String fromAddress, boolean isSSL, boolean enableSTARTTLS) {
+
+		//DEBUG
+		System.out.println("Setting up emailbroadcaster with smtpUrl: "+smtpUrl);
+		System.out.println("Setting up emailbroadcaster with username: "+username);
+		System.out.println("Setting up emailbroadcaster with password: "+password);
+		System.out.println("Setting up emailbroadcaster with fromAddress: "+fromAddress);
+		System.out.println("Setting up emailbroadcaster with isSSL: "+isSSL);
+		System.out.println("Setting up emailbroadcaster with enableSTARTTLS: "+enableSTARTTLS);
+		//DEBUG
+
 		fromEmail = fromAddress;
 		Properties props = new Properties();
-		
+
 		String[] parts = smtpUrl.split(":");
 		String smtpAdd = parts[0];
 		String smtpPort = (parts.length > 1) ? parts[1] : "465";
-		
+
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.host", smtpAdd);
-		if (smtpAdd.contains("gmail") || smtpAdd.contains("live.com")) {
+		if ((smtpAdd.contains("gmail") || smtpAdd.contains("live.com")) || enableSTARTTLS) {
 			props.put("mail.smtp.starttls.enable", "true");
 		}
-		
+
 		if (isSSL) {
 			props.put("mail.smtp.socketFactory.port", smtpPort);
 			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		}
-		
-		//props.put("mail.smtp.socketFactory.port", smtpPort );
-		//props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+
 		props.put("mail.smtp.port", smtpPort);
-		
+
 		if (username != null && username.trim().length() > 0) {
 			props.put("mail.smtp.auth", "true");
 			session = Session.getInstance(props, new javax.mail.Authenticator() {
@@ -84,12 +91,14 @@ public class EmailBroadcaster {
 		} else {
 			props.put("mail.smtp.auth", "false");
 			props.put("mail.smtp.auth.login.disable", "true");
-			
+
 			session = Session.getInstance(props, null);
 		}
-		
+
+		System.out.println("Finished setting up emailbroadcaster object");
+
 	}
-	
+
 	///
 	/// Full featured sendEmail function, all other "overloaded" functions are convinence functiosn based on this
 	///
@@ -105,14 +114,14 @@ public class EmailBroadcaster {
 		String bccAddresses[], Map<String, String> fileAttachments, String fromAddress) throws Exception {
 		// Actual message contianer used by the function
 		MimeMessage message = new MimeMessage(session);
-		
+
 		// Process "FROM" address field
 		if (fromAddress != null) {
 			message.setFrom(new InternetAddress(fromAddress));
 		} else {
 			message.setFrom(new InternetAddress(fromEmail));
 		}
-		
+
 		// Process "TO" address field
 		if (toAddresses == null || toAddresses.length <= 0) {
 			throw new Exception("Sending to email address is not allowed to be 'empty'");
@@ -122,7 +131,7 @@ public class EmailBroadcaster {
 			addressTo[i] = new InternetAddress(toAddresses[i]);
 		}
 		message.setRecipients(RecipientType.TO, addressTo);
-		
+
 		// Process "CC" address field
 		if (ccAddresses != null && ccAddresses.length > 0) {
 			InternetAddress[] addressCC = new InternetAddress[ccAddresses.length];
@@ -133,7 +142,7 @@ public class EmailBroadcaster {
 				message.setRecipients(RecipientType.CC, addressCC);
 			}
 		}
-		
+
 		// Process "BCC" address field
 		if (bccAddresses != null && bccAddresses.length > 0) {
 			InternetAddress[] addressBCC = new InternetAddress[bccAddresses.length];
@@ -144,50 +153,50 @@ public class EmailBroadcaster {
 				message.setRecipients(RecipientType.BCC, addressBCC);
 			}
 		}
-		
+
 		// Set email SUBJECT
 		message.setSubject(subject);
-		
+
 		// Message container (for body, and attachments)
 		Multipart multipart = new MimeMultipart();
-		
+
 		// Set email HTML CONTENT
 		if (htmlContent != null) {
 			MimeBodyPart messageBodyPart = new MimeBodyPart();
 			messageBodyPart.setContent(htmlContent, "text/html");
 			multipart.addBodyPart(messageBodyPart);
 		}
-		
+
 		// Loops through file attachments, and add it
 		if (fileAttachments != null) {
 			for (Map.Entry<String, String> entry : fileAttachments.entrySet()) {
 				MimeBodyPart messageBodyPart = new MimeBodyPart();
 				String key = entry.getKey();
 				String value = entry.getValue();
-				
+
 				DataSource source = new FileDataSource(value);
 				messageBodyPart.setDataHandler(new DataHandler(source));
 				messageBodyPart.setFileName(key);
 				multipart.addBodyPart(messageBodyPart);
 			}
 		}
-		
+
 		// Apply message content
 		message.setContent(multipart);
-		
+
 		//Sends the message
 		Transport.send(message);
-		
+
 		return true;
 	}
-	
+
 	/// Shorten convinence function (does not need testing, test the core function directly instead)
 	public boolean sendEmail(String subject, String htmlContent, String toAddresses[], String ccAddresses[],
 		String bccAddresses[], HashMap<String, String> fileAttachments) throws Exception {
 		return sendEmail(subject, htmlContent, toAddresses, ccAddresses, bccAddresses, fileAttachments, null);
-		
+
 	}
-	
+
 	/// Shorten convinence function (does not need testing, test the core function directly instead)
 	public boolean sendEmail(String subject, String htmlContent, String toAddresses, String ccAddresses,
 		String bccAddresses, HashMap<String, String> fileAttachments) throws Exception {
@@ -195,12 +204,12 @@ public class EmailBroadcaster {
 			((ccAddresses != null) ? (ccAddresses.split(",")) : null), ((bccAddresses != null) ? (bccAddresses.split(","))
 				: null), fileAttachments, null);
 	}
-	
+
 	/// Shorten convinence function (does not need testing, test the core function directly instead)
 	public boolean sendEmail(String subject, String htmlContent, String toAddresses,
 		HashMap<String, String> fileAttachments) throws Exception {
 		return sendEmail(subject, htmlContent, ((toAddresses != null) ? (toAddresses.split(",")) : null), null, null,
 			fileAttachments, null);
 	}
-	
+
 }

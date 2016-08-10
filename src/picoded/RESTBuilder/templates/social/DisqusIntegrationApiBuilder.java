a9dbc0af19be1e6365317b94e996cc3137732f2c
Eugene Cheah <eugeneqin@gmail.com>
2016-08-11 03:16:34 +0800

    Disqus integration API class to build

---
+++ src/picoded/RESTBuilder/templates/social/DisqusIntegrationApiBuilder.java
@@ -0,0 +1,9 @@
+package picoded.RESTBuilder.templates.social;
+
+import picoded.RESTBuilder.RESTBuilder;
+import picoded.RESTBuilder.RESTFunction;
+import picoded.enums.HttpRequestType;
+
+public class DisqusIntegrationApiBuilder {
+	
+}

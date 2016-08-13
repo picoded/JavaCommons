65569772270ffcd6af48b173cbc6a813ec8e224e
Eugene Cheah <eugeneqin@gmail.com>
2016-08-14 01:26:11 +0800

    skeleton for S3Uploader (to port from orgeva)

---
+++ src/picoded/RESTBuilder/templates/cloud/S3UploaderApiBuilder.java
@@ -0,0 +1,9 @@
+package picoded.RESTBuilder.templates.cloud;
+
+import picoded.RESTBuilder.RESTBuilder;
+import picoded.RESTBuilder.RESTFunction;
+import picoded.enums.HttpRequestType;
+
+public class S3UploaderApiBuilder {
+	
+}

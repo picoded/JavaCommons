ba9806e66fe43d485eb60a9be92995f94cd0bf32
Samuel Barnard <samuel@socialoctet.com>
2016-08-23 13:49:42 +0800

    Updated disqus rest to have login and logout

--- src/picoded/RESTBuilder/template/social/DisqusIntegration.java
+++ src/picoded/RESTBuilder/template/social/DisqusIntegration.java
@@ -4,17 +4,20 @@ import picoded.RESTBuilder.RESTBuilder;
 import picoded.RESTBuilder.RESTFunction;
 import picoded.enums.HttpRequestType;
 import picoded.servlet.CommonsPage;
+import picoded.servlet.BasePage;
 import picoded.conv.ConvertJSON;
+import picoded.JStruct.AccountTable;
+import picoded.JStruct.AccountObject;
 
 import java.util.Base64;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Formatter;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.Mac;
-
-// import org.codehaus.jackson.map.ObjectMapper;
 
 public class DisqusIntegration {
 
@@ -22,18 +25,9 @@ public class DisqusIntegration {
 
 	}
 
-	///
 	/// Generates disqus SSO information
 	///
 	/// ## HTTP Request Parameters
-	///
-	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
-	/// | Parameter Name  | Variable Type	   | Description                                                                   |
-	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
-	/// | _oid            | String             | object ID used to generate the string. If no oid is given, return null.       |
-	/// | username        | String             | Username used to generate the string. If no username is given, return null.   |
-	/// | email           | String             | Email used to generate the string. If no email is given, return null.         |
-	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
 	///
 	/// ## JSON Object Output Parameters
 	///
@@ -45,48 +39,109 @@ public class DisqusIntegration {
 	/// | error           | String (Optional)  | Errors encounted if any                                                       |
 	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
 	///
-	public RESTFunction sso_hmac_sha1 = (req, res) -> {
+	public RESTFunction sso_login = (req, res) -> {
 		res.put("error", null);
 		res.put("hmac_sha1", null);
 		res.put("restLog", "");
 
-		String oid = req.getString("_oid", "");
-		if(oid.isEmpty()){
-			res.put("error", "No _oid specified, unable to generate hmac_sha1");
-			return res;
+		List<String> restLog = new ArrayList<String>();
+
+		BasePage bp = (BasePage)req.requestPage();
+		AccountTable at = bp.accountAuthTable();
+		AccountObject ao = at.getRequestUser(bp.getHttpServletRequest());
+
+		if(ao == null){
+			res.put("restLog", "User is not logged in.");
+		}else{
+			String oid = ao._oid();
+			if(oid == null || oid.isEmpty()){
+				res.put("error", "No oid found for user.");
+				return res;
+			}
+
+			String username = ao.getString("username", "test_user"); //change me
+			String email = ao.getString("email", "test_user@mailinator.com"); //change me
+
+			String secretKey = bp.JConfig().getString("site.apiKey.disqus.secretKey", "");
+			if(secretKey.isEmpty()){
+				res.put("error", "No secret key found, unable to generate hmac_sha1 for disqus login");
+				return res;
+			}
+
+			String publicKey = bp.JConfig().getString("site.apiKey.disqus.publicKey", "");
+			if(publicKey.isEmpty()){
+				res.put("error", "No public key found, unable to generate hmac_sha1 for disqus login");
+				return res;
+			}
+			res.put("publicKey", publicKey);
+
+			Map<String, String> message = new HashMap<String, String>();
+			message.put("id", oid);
+			message.put("username", username);
+			message.put("email", email);
+
+			String signature = "";
+			try{
+				signature = sso_hmac_sha1(message, secretKey);
+			}catch(Exception e){
+				res.put("error", e.getMessage());
+				return res;
+			}
+			res.put("hmac_sha1", signature);
 		}
 
-		String username = req.getString("username", "");
-		if(username.isEmpty()){
-			res.put("error", "No username specified, unable to generate hmac_sha1");
-			return res;
-		}
+		res.put("restLog", restLog);
+		return res;
+	};
 
-		String email = req.getString("email", "");
-		if(email.isEmpty()){
-			res.put("error", "No email specified, unable to generate hmac_sha1");
-			return res;
-		}
+	/// Generates disqus SSO information
+	///
+	/// ## HTTP Request Parameters
+	///
+	/// ## JSON Object Output Parameters
+	///
+	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
+	/// | Parameter Name  | Variable Type	   | Description                                                                   |
+	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
+	/// | hmac_sha1       | String             | The generated hmac_sha1 string for SSO.                                       |
+	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
+	/// | error           | String (Optional)  | Errors encounted if any                                                       |
+	/// +-----------------+--------------------+-------------------------------------------------------------------------------+
+	///
+	public RESTFunction sso_logout = (req, res) -> {
+		res.put("error", null);
+		res.put("hmac_sha1", null);
+		res.put("restLog", "");
 
-		CommonsPage corePageObj = (CommonsPage)(req.requestPage());
-		String secretKey = corePageObj.JConfig().getString("site.apiKey.disqus.secretKey", "");
+		BasePage bp = (BasePage)req.requestPage();
+
+		String secretKey = bp.JConfig().getString("site.apiKey.disqus.secretKey", "");
 		if(secretKey.isEmpty()){
-			res.put("error", "No secret key found, unable to generate hmac_sha1");
+			res.put("error", "No secret key found, unable to generate hmac_sha1 for disqus logout");
 			return res;
 		}
 
-		String publicKey = corePageObj.JConfig().getString("site.apiKey.disqus.publicKey", "");
+		String publicKey = bp.JConfig().getString("site.apiKey.disqus.publicKey", "");
 		if(publicKey.isEmpty()){
-			res.put("error", "No public key found, unable to generate hmac_sha1");
+			res.put("error", "No public key found, unable to generate hmac_sha1 for disqus logout");
 			return res;
 		}
 		res.put("publicKey", publicKey);
 
 		Map<String, String> message = new HashMap<String, String>();
-		message.put("id", oid);
-		message.put("username", username);
-		message.put("email", email);
+		String signature = "";
+		try{
+			signature = sso_hmac_sha1(message, secretKey);
+		}catch(Exception e){
+			res.put("error", e.getMessage());
+			return res;
+		}
+		res.put("hmac_sha1", signature);
 
+		return res;
+	};
+
+	private static String sso_hmac_sha1(Map<String, String> message, String secretKey) throws Exception{
 		//convert message object to json string, then base64 encode it
 		String jsonMessage = ConvertJSON.fromObject(message);
 		String base64EncodedStr = new String(Base64.getEncoder().encodeToString(jsonMessage.getBytes()));
@@ -94,17 +149,9 @@ public class DisqusIntegration {
 		long timestamp = System.currentTimeMillis()/1000;
 
 		// Assemble the HMAC-SHA1 signature
-		String signature = "";
-		try{
-			signature = calculateRFC2104HMAC(base64EncodedStr + " " + timestamp, secretKey);
-		}catch(Exception e){
-			res.put("error", e.getMessage());
-			return res;
-		}
-
-		res.put("hmac_sha1", signature);
-		return res;
-	};
+		String signature = calculateRFC2104HMAC(base64EncodedStr + " " + timestamp, secretKey);
+		return base64EncodedStr + " " + signature + " " + timestamp;
+	}
 
 	private static String toHexString(byte[] bytes)
 	{
@@ -127,7 +174,11 @@ public class DisqusIntegration {
 	}
 
 	public RESTBuilder setupRestBuilder(RESTBuilder rbObj, String setPrefix) {
-		rbObj.getNamespace(setPrefix + "sso_hmac_sha1").put(HttpRequestType.GET, sso_hmac_sha1);
+		rbObj.getNamespace(setPrefix + "sso_login").put(HttpRequestType.GET, sso_login);
+		rbObj.getNamespace(setPrefix + "sso_login").put(HttpRequestType.POST, sso_login);
+
+		rbObj.getNamespace(setPrefix + "sso_logout").put(HttpRequestType.GET, sso_logout);
+		rbObj.getNamespace(setPrefix + "sso_logout").put(HttpRequestType.POST, sso_logout);
 
 		return rbObj;
 	}

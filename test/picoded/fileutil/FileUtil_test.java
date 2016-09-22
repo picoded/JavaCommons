12b5bab46b8c754580f025db36ec78c92d1140b5
Rashmi Singh <rashmis19oct@gmail.com>
2016-09-22 17:59:34 +0530

    T604 - FileUtil - implmentation refactoring

--- test/picoded/fileutil/FileUtil_test.java
+++ test/picoded/fileutil/FileUtil_test.java
@@ -164,11 +164,6 @@ public class FileUtil_test {
 		FileUtil.copyDirectory_ifDifferent(new File("./test-files/test-specific/fileutils/ConfigFile/"),
 				new File("./test-files/tmp/"), true, false);
 		
-		FileUtil.copyDirectory_ifDifferent(null, null, false, true);
-		FileUtil.copyDirectory_ifDifferent(new File(""), new File(""), false, true);
-		FileUtil.copyDirectory_ifDifferent(testDir, outputDir, false, true);
-		FileUtil.copyDirectory_ifDifferent(new File("./test-files/test-specific/fileutils/"), new File(
-			"./test-files/tmp/"), false, true);
 		FileUtil.copyDirectory_ifDifferent(new File("./test-files/test-specific/fileutils/ConfigFile/"),
 				new File("./test-files/tmp/"), false, true);
 		FileUtil.copyDirectory_ifDifferent(new File(""),
@@ -185,7 +180,6 @@ public class FileUtil_test {
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "jsRegex.js"), new File(outputDirStr+ "jsRegex.js"));
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "jsRegex.js"), new File(outputDirStr+ "jsRegex.js"), true);
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "jsRegex.js"), new File(outputDirStr+ "jsRegex.js"), true, false);
-		FileUtil.copyFile_ifDifferent(new File(testDirStr + "jsRegex.js"), new File(outputDirStr+ "jsRegex.js"), false, true);
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "jsRegex.js"), new File(outputDirStr+ "jsRegex.js"), true, false);
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "doubleSlash.txt"), new File(outputDirStr+ "doubleSlash.txt"), true, false);
 		FileUtil.copyFile_ifDifferent(new File(testDirStr + "doubleSlash.txt"), new File(outputDirStr+ "doubleSlash.txt"), true);

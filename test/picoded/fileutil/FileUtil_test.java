fe6561911ab8b8c2e316760ae1343a886ee44576
Rashmi Singh <rashmis19oct@gmail.com>
2016-09-28 11:34:10 +0530

    T604 - FileUtil - implmentation refactoring

--- test/picoded/fileutil/FileUtil_test.java
+++ test/picoded/fileutil/FileUtil_test.java
@@ -202,8 +202,7 @@ public class FileUtil_test {
 		Path symLinkPath = Paths.get(outputDirStr+ "jsRegexLink.js");
 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile());
 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile(), true);
-		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), true);
-		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile(), true, false);
+		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), true, false);
 		symLinkPath.toFile().delete();
 		existingFilePath = Paths.get(testDirStr + "doubleSlash.txt");
 		symLinkPath = Paths.get(outputDirStr+ "doubleSlashLink.txt");

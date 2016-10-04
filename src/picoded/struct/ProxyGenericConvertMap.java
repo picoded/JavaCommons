ea383b27973fa680a0547bfd87f24faead510e05
Eugene Cheah <eugene@picoded.com>
2016-10-04 13:03:54 +0800

    Revert "Removed generic convert, to make build able - not required for Core servlet page"
    This reverts commit ff2595c4b56bef4fe9186a31d0e278ba0af1cda8.

---
+++ src/picoded/struct/ProxyGenericConvertMap.java
@@ -0,0 +1,46 @@
+package picoded.struct;
+
+import java.util.Map;
+
+import org.apache.commons.collections4.map.AbstractMapDecorator;
+
+public class ProxyGenericConvertMap <K, V> extends AbstractMapDecorator<K, V> implements GenericConvertMap<K, V> {
+
+	// ------------------------------------------------------
+	//
+	// Constructors
+	//
+	// ------------------------------------------------------
+
+	// / Consturctor
+	public ProxyGenericConvertMap() {
+		super();
+	}
+
+	// / Consturctor
+	@SuppressWarnings("unchecked")
+	public ProxyGenericConvertMap(Map<? extends K, ? extends V> m) {
+		super((Map<K, V>) m);
+	}
+	
+	// / The static builder for the map
+	public static <A, B> GenericConvertMap<A, B> ensure(Map<A, B> inMap) {
+		if (inMap instanceof GenericConvertMap) { // <A,B>
+			return (GenericConvertMap<A, B>) inMap;
+		}
+		return (new ProxyGenericConvertMap<A, B>(inMap));
+	}
+
+	// ------------------------------------------------------
+	//
+	// Overwrites
+	//
+	// ------------------------------------------------------
+
+	// / Implments a JSON to string conversion
+	@Override
+	public String toString() {
+		//TODO return GenericConvert.toString((Object) this);
+		return null;
+	}
+}

905ebf92ebe426382ff03de3ae89d5fd1fa514e5
Rahsmi Singh <rashmis19oct@gmail.com>
2016-10-06 16:13:27 +0530

    T604 - FileUtil - implmentation refactoring

--- test/picoded/file/FileUtil_test.java
+++ test/picoded/file/FileUtil_test.java
@@ -201,18 +201,19 @@ public class FileUtil_test {
 	public void testCopyFileIfDifferent() throws IOException { 		
  		Path existingFilePath = Paths.get(testDirStr + "jsRegex.js");
  		Path symLinkPath = Paths.get(outputDirStr + "jsRegexLink.js");
+ 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile());
  		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile());
  		
  		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile(), true);
  		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(), 
  				symLinkPath.toFile(), false);
+ 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), 
+ 				Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
+ 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), 
+ 				Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
  		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), false);
  		
  		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile(), false, false);
- 		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(),
- 				symLinkPath.toFile(), false, false);
- 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), false, false);
-
 	}
 	
 	@Test

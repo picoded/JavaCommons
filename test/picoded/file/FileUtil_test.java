0275aa209aa99eb52d7544a3d6b0b674ae01d853
Rahsmi Singh <rashmis19oct@gmail.com>
2016-10-06 13:17:29 +0530

    T604 - FileUtil - implmentation refactoring

--- test/picoded/file/FileUtil_test.java
+++ test/picoded/file/FileUtil_test.java
@@ -202,25 +202,18 @@ public class FileUtil_test {
 		Path existingFilePath = Paths.get(testDirStr + "jsRegex.js");
 		Path symLinkPath = Paths.get(outputDirStr + "jsRegexLink.js");
 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile());
+		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), existingFilePath.toFile());
 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile());
 		
+		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), false);
 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), existingFilePath.toFile(), false);
-		
 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), existingFilePath.toFile(), false);
 		
-		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(), 
-				existingFilePath.toFile(), false);
-		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(), 
-				symLinkPath.toFile(), false);
-		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), 
-				Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
-		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), 
-				Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
-		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), false);
+		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
+		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), Paths.get(testDirStr + "doubleSlash.txt").toFile(), false);
 		
 		FileUtil.copyFile_ifDifferent(existingFilePath.toFile(), symLinkPath.toFile(), false, false);
-		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(),
-				symLinkPath.toFile(), false, false);
+		FileUtil.copyFile_ifDifferent(Paths.get(testDirStr + "doubleSlash.txt").toFile(), symLinkPath.toFile(), false, false);
 		FileUtil.copyFile_ifDifferent(symLinkPath.toFile(), symLinkPath.toFile(), false, false);
 	}
 	

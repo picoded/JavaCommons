471081e4f4561b5ecfdef00af1b0269cdbe4996e
Rahsmi Singh <rashmis19oct@gmail.com>
2016-10-13 15:25:06 +0530

    RC-WorkingBranch-struc

--- src/picoded/struct/HashMapList.java
+++ src/picoded/struct/HashMapList.java
@@ -7,12 +7,13 @@ import picoded.conv.MapValueConv;
 ///
 /// Convinence class, for creating a Map containing list values, and appending them
 ///
-/// Implement the GenericConvertMap interface, once that class is transfered to RC
+/// @TODO : Implement the GenericConvertMap interface, once that class is transfered to RC
 ///
-public class HashMapList<K, V> extends HashMap<K, List<V>> implements GenericConvertMap<K, List<V>> {
-
+public class HashMapList<K, V> extends HashMap<K, List<V>> /* implements GenericConvertMap<K, List<V>> */{
+	
 	/// "Serializable" classes should have a version id 
 	private static final long serialVersionUID = 1L;
+	
 	/// Appends the value to the inner list, creating a new ArrayList if needed
 	///
 	/// @Parameter   key     key to use
@@ -20,14 +21,17 @@ public class HashMapList<K, V> extends HashMap<K, List<V>> implements GenericCon
 	///
 	/// @Returns   returns itself
 	public HashMapList<K, V> append(K key, V value) {
+		
 		// Get or create list
 		List<V> valArr = this.get(key);
 		if (valArr == null) {
 			valArr = new ArrayList<V>();
 		}
+		
 		// Add and update list
 		valArr.add(value);
 		this.put(key, valArr);
+		
 		// Returns self
 		return this;
 	}

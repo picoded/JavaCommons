6cb074ea0f7f1b0efa8ac8fe299d807b52bd67ab
Dheeraj Singh <blstechds@gmail.com>
2016-11-07 12:13:43 +0530

    T799 - un-used method removed

--- test/picoded/servlet/CorePageExtended.java
+++ test/picoded/servlet/CorePageExtended.java
@@ -5,35 +5,10 @@ import java.util.Map;
 
 import javax.servlet.ServletException;
 
-public class CorePageExtended extends CorePage {
-	
+public class CorePageExtended extends CorePage{
+
 	private static final long serialVersionUID = 1L;
-	
-	public CorePageExtended() {
-		//super();
-	}
-	
-	@Override
-	public void initializeContext() throws Exception {
-		super.initializeContext();
-	}
-	
-	@Override
-	public void doSharedTeardown() throws Exception {
-		super.doSharedTeardown();
-	}
-	
-	//	@Override
-	//	public boolean processChain() throws ServletException {
-	//		httpResponse.setContentType("application/text");
-	////		try {
-	////			httpResponse.getWriter().append(super.getContextURI());
-	////		} catch (IOException e) {
-	////			throw new ServletException(e);
-	////		}
-	//		return true;
-	//	}
-	
+
 	@Override
 	public boolean doPostRequest(Map<String, Object> templateData) throws Exception {
 		try {

4fd6bc0716033cd3098ceaf876734ca62ddeadc0
Eugene Cheah <eugeneqin@gmail.com>
2016-11-10 14:27:15 +0800

    CovnvertXML class - to convert XML strings to Map structures

--- src/picoded/conv/ConvertXML.java
+++ src/picoded/conv/ConvertXML.java
@@ -20,6 +20,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 
+// JavaCommons libs used
+import picoded.conv.GenericConvert;
+
 ///
 /// XML simplification helpers. When you do not need custom object / array structures
 ///
@@ -58,7 +61,7 @@ public class ConvertXML {
 	/// @param  XML string to use
 	///
 	/// @return   Document object to return
-	public static Document toDocument(String xmlStr) {
+	protected static Document toDocument(String xmlStr) {
 		try {
 			// Document builder
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -80,7 +83,7 @@ public class ConvertXML {
 	/// @param  XML string to use
 	///
 	/// @return   Document Element object to return
-	public static Element toElement(String xmlStr) {
+	protected static Element toElement(String xmlStr) {
 		return toDocument(xmlStr).getDocumentElement();
 	}
 	
@@ -92,7 +95,7 @@ public class ConvertXML {
 	/// @param  XML string to use
 	///
 	/// @return   Result map
-	public static Map<String,Object> toMap(Element el) {
+	protected static Map<String,Object> toMap(Element el) {
 		Map<String,Object> ret = new HashMap<String,Object>();
 		
 		// Process element
@@ -110,7 +113,7 @@ public class ConvertXML {
 			//
 			// Process tag name
 			//
-			ret.put("TagName", el.getTagName());
+			ret.put("TagName", el.getTagName().toLowerCase());
 			
 			//
 			// Processs children nodes
@@ -138,14 +141,17 @@ public class ConvertXML {
 				if( list.size() > 0 ) {
 					ret.put("ChildNodes", list);
 				}
-				
 				if( nodeValue.length() > 0 ) {
 					ret.put("NodeValue", nodeValue);
 				}
+			} else {
+				String nodeValue = el.getNodeValue();
+				if(nodeValue == null || nodeValue.length() <= 0) {
+					nodeValue = el.getTextContent();
+				}
+				ret.put("NodeValue", nodeValue);
 			}
-			
 		}
-		
 		return ret;
 	}
 	
@@ -158,4 +164,118 @@ public class ConvertXML {
 		return toMap( toElement(xmlStr) );
 	}
 	
+	/// Inserts a child map, in accordence to its type
+	/// 
+	/// @param  The base map to build on
+	/// @param  The child node to add to base map
+	///
+	/// @return  The base map
+	@SuppressWarnings("unchecked")
+	protected static Map<String,Object> addChildNodeToBaseMap(Map<String,Object>baseMap, Map<String,Object>childMap) {
+		//
+		// Get the type key
+		//
+		String type = GenericConvert.toString( childMap.get("type"), null );
+		if( type == null || type.length() <= 0 ) {
+			type = "child";
+		}
+		
+		//
+		// Get existing value associated with the type key
+		//
+		Object baseMapOri = baseMap.get(type);
+		
+		//
+		// Base map : if blank, assume first node
+		//
+		if( baseMapOri == null ) {
+			baseMap.put(type, childMap);
+			return baseMap;
+		}
+		
+		//
+		// List map to save child nodes to?
+		//
+		ArrayList<Object> listToSaveTo = null;
+		if( baseMapOri instanceof ArrayList ) {
+			listToSaveTo = (ArrayList<Object>)baseMapOri;
+		} else {
+			listToSaveTo = new ArrayList<Object>();
+			listToSaveTo.add( baseMapOri );
+		}
+		
+		//
+		// Saving child map
+		//
+		listToSaveTo.add(childMap);
+		baseMap.put(type, listToSaveTo);
+		
+		//
+		// Return the base map
+		//
+		return baseMap;
+	}
+	
+	/// Collapse an XML map into a more JSON concise format.
+	/// Assuming no key and tag name conflict.
+	///
+	/// This collapse the items along the key words "ChildNodes", "NodeValue", "TagName"
+	///
+	/// @param  The Map to collapse
+	///
+	/// @return  Result map
+	public static Map<String,Object> toCollapsedMap(Map<String,Object> inMap) {
+		
+		//
+		// Normalize "NodeValue" as "value"
+		//
+		String nodeValue = GenericConvert.toString( inMap.get("NodeValue"), null );
+		if( nodeValue != null && nodeValue.length() > 0 ) {
+			inMap.put("value", nodeValue);
+		}
+		inMap.remove("NodeValue");
+		
+		//
+		// Normalize "TagName" as "type"
+		//
+		String tagName = GenericConvert.toString( inMap.get("TagName"), null );
+		if(tagName != null && tagName.length() > 0) {
+			inMap.put("type", tagName);
+		}
+		inMap.remove("TagName");
+		
+		//
+		// Normalize "ChildNodes"
+		//
+		List<Object> childList = GenericConvert.toList( inMap.get("ChildNodes"), null );
+		inMap.remove("ChildNodes");
+		if( childList != null && childList.size() > 0 ) {
+			for(int i=0; i<childList.size(); ++i) {
+				// Child node to process
+				Map<String,Object> childNode = GenericConvert.toStringMap( childList.get(i) );
+				
+				// Recusion filter
+				childNode = toCollapsedMap(childNode);
+				
+				// Child node adding
+				addChildNodeToBaseMap(inMap, childNode);
+			}
+		}
+		
+		// Return normalized map
+		return inMap;
+	}
+	
+	
+	/// Collapse an XML map into a more JSON concise format.
+	/// Assuming no key and tag name conflict.
+	///
+	/// This collapse the items along the key words "ChildNodes", "NodeValue", "TagName"
+	///
+	/// @param  The XML String to collapse
+	///
+	/// @return  Result map
+	public static Map<String,Object> toCollapsedMap(String xmlStr) {
+		return toCollapsedMap( toMap(xmlStr) );
+	}
 }

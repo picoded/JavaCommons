d5c3031e146a201e5ec2a649fc51d487b69893c6
Rahsmi Singh <rashmis19oct@gmail.com>
2016-11-14 15:52:01 +0530

    T908 - AccountTable - implmentation refactoring

---
+++ test/picoded/JStruct/JStruct_test.java
@@ -0,0 +1,76 @@
+package picoded.JStruct;
+
+// Target test class
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+// Test Case include
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JStruct_test {
+	
+	private static JStruct jStructObj = new JStruct();
+	private static KeyValueMap keyValueMap;
+	private static MetaTable metaTable;
+	private static AccountTable accountTable;
+	
+	@BeforeClass
+	public static void setUp() {
+		jStructObj.systemSetup();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		jStructObj.systemTeardown();
+		jStructObj = null;
+	}
+	
+	@Test
+	public void getKeyValueMapTest1() {
+		keyValueMap = jStructObj.getKeyValueMap("test");
+		assertNotNull(keyValueMap);
+		assertTrue(keyValueMap.isEmpty());
+	}
+	
+	@Test
+	public void getKeyValueMapTest2() {
+		KeyValueMap keyValueMapLocal = jStructObj.getKeyValueMap("test");
+		assertNotNull(keyValueMapLocal);
+		assertEquals(keyValueMap, keyValueMapLocal);
+	}
+	
+	@Test
+	public void getMetaTableTest1() {
+		metaTable = jStructObj.getMetaTable("test");
+		assertNotNull(metaTable);
+		assertTrue(metaTable.isEmpty());
+	}
+	
+	@Test
+	public void getMetaTableTest2() {
+		MetaTable metaTableLocal = jStructObj.getMetaTable("test");
+		assertNotNull(metaTableLocal);
+		assertEquals(metaTable, metaTableLocal);
+	}
+	
+	@Test
+	public void getAccountTableTest1() {
+		accountTable = jStructObj.getAccountTable("test");
+		assertNotNull(accountTable);
+		assertTrue(accountTable.isEmpty());
+	}
+	
+	@Test
+	public void getAccountTableTest2() {
+		AccountTable accountTableLocal = jStructObj.getAccountTable("test");
+		assertNotNull(accountTableLocal);
+		assertEquals(accountTable, accountTableLocal);
+	}
+	
+}
---
+++ test/picoded/JStruct/KeyValueMap_test.java
@@ -0,0 +1,151 @@
+package picoded.JStruct;
+
+// Target test class
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.After;
+// Test Case include
+import org.junit.Before;
+import org.junit.Test;
+
+// Test depends
+
+public class KeyValueMap_test {
+	
+	/// Test object
+	public KeyValueMap kvmObj = null;
+	
+	/// To override for implementation
+	///------------------------------------------------------
+	public KeyValueMap implementationConstructor() {
+		return (new JStruct()).getKeyValueMap("test");
+	}
+	
+	/// Setup and sanity test
+	///------------------------------------------------------
+	@Before
+	public void setUp() {
+		kvmObj = implementationConstructor();
+		kvmObj.systemSetup();
+	}
+	
+	@After
+	public void tearDown() {
+		if (kvmObj != null) {
+			kvmObj.systemTeardown();
+		}
+		kvmObj = null;
+	}
+	
+	@Test
+	public void constructorTest() {
+		//not null check
+		assertNotNull(kvmObj);
+		
+		//run maintaince, no exception?
+		kvmObj.maintenance();
+	}
+	
+	/// utility
+	///------------------------------------------------------
+	public long currentSystemTimeInSeconds() {
+		return (System.currentTimeMillis() / 1000L);
+	}
+	
+	/// basic test
+	///------------------------------------------------------
+	
+	@Test
+	public void simpleHasPutHasGet() throws Exception {
+		assertFalse(kvmObj.containsKey("hello"));
+		kvmObj.put("hello", "world");
+		assertEquals("world", kvmObj.get("hello"));
+		assertTrue(kvmObj.containsKey("hello"));
+		assertEquals("world", kvmObj.get("hello"));
+	}
+	
+	@Test
+	public void getExpireTime() throws Exception {
+		long expireTime = currentSystemTimeInSeconds() * 2;
+		kvmObj.putWithExpiry("yes", "no", expireTime);
+		
+		assertNotNull(kvmObj.getExpiry("yes"));
+		assertEquals(expireTime, kvmObj.getExpiry("yes"));
+	}
+	
+	@Test
+	public void setExpireTime() throws Exception {
+		long expireTime = currentSystemTimeInSeconds() * 2;
+		kvmObj.putWithExpiry("yes", "no", expireTime);
+		
+		long newExpireTime = kvmObj.getExpiry("yes") * 2;
+		kvmObj.setExpiry("yes", newExpireTime);
+		
+		long fetchedExpireTime = kvmObj.getExpiry("yes");
+		
+		assertNotNull(fetchedExpireTime);
+		assertEquals(fetchedExpireTime, newExpireTime);
+	}
+	
+	@Test
+	public void setLifeSpan() throws Exception {
+		long lifespanTime = 4 * 24 * 60 * 60 * 60;
+		kvmObj.putWithLifespan("yes", "no", lifespanTime);
+		
+		long newLifespanTime = kvmObj.getExpiry("yes");
+		kvmObj.setLifeSpan("yes", newLifespanTime);
+		
+		assertNotNull(kvmObj.getLifespan("yes"));
+	}
+	
+	@Test
+	public void testColumnExpiration() throws Exception {
+		//set column expiration time to current time + 30 secs.
+		long expirationTime = currentSystemTimeInSeconds() + 1;
+		kvmObj.putWithExpiry("yes", "no", expirationTime);
+		
+		//before the expiration time key will not be null.
+		assertNotNull(kvmObj.get("yes"));
+		
+		//sleep the execution for 31 secs so that key gets expired.
+		Thread.sleep(2000);
+		
+		//key should be null after expiration time.
+		assertEquals(null, kvmObj.get("yes"));
+	}
+	
+	@Test
+	public void getKeysTest() throws Exception {
+		assertEquals(new HashSet<String>(), kvmObj.getKeys("world"));
+		
+		kvmObj.put("yes", "no");
+		kvmObj.put("hello", "world");
+		kvmObj.put("this", "world");
+		kvmObj.put("is", "sparta");
+		
+		assertEquals("no", kvmObj.get("yes"));
+		assertEquals("world", kvmObj.get("hello"));
+		assertEquals("world", kvmObj.get("this"));
+		assertEquals("sparta", kvmObj.get("is"));
+		
+		assertEquals(new HashSet<String>(Arrays.asList(new String[] { "hello", "this" })),
+			kvmObj.getKeys("world"));
+	}
+	
+	/// nonce test
+	///------------------------------------------------------
+	
+	@Test
+	public void simpleNonce() {
+		String nonce;
+		assertNotNull(nonce = kvmObj.generateNonce("hello"));
+		assertEquals("hello", kvmObj.get(nonce));
+	}
+	
+}

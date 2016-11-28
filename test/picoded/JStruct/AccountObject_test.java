e024e61a69802cddd02d18f38b9448aabb8b4b02
Rahsmi Singh <rashmis19oct@gmail.com>
2016-11-28 18:36:17 +0530

    T908 - AccountObject - implmentation refactoring

---
+++ test/picoded/JStruct/AccountObject_test.java
@@ -0,0 +1,86 @@
+package picoded.JStruct;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import picoded.JStruct.internal.JStruct_MetaTable;
+
+public class AccountObject_test {
+	// / Test object
+	public AccountTable accountTable = null;
+	public AccountObject accountObject = null;
+
+	// / To override for implementation
+	// /------------------------------------------------------
+	public AccountTable implementationConstructor() {
+		return (new JStruct()).getAccountTable("test");
+	}
+
+	// / Setup and sanity test
+	// /------------------------------------------------------
+	@Before
+	public void setUp() {
+		accountTable = implementationConstructor();
+		accountTable.systemSetup();
+		String grpName = "hello-group";
+		assertFalse(accountTable.containsKey(grpName));
+		assertNull(accountTable.get(grpName));
+		accountTable.systemSetup();
+		accountObject = accountTable.newObject();
+		JStruct_MetaTable jStruct_MetaTable = (JStruct_MetaTable) (new JStruct())
+				.getMetaTable("test");
+		accountObject = new AccountObject(accountTable, jStruct_MetaTable,
+				accountObject._oid(), true);
+	}
+
+	@After
+	public void tearDown() {
+		if (accountTable != null) {
+			accountTable.systemTeardown();
+		}
+		accountTable = null;
+	}
+
+	@Test
+	public void setPasswordTest() {
+		assertTrue(accountObject.setPassword(null));
+		assertTrue(accountObject.setPassword("test@123"));
+		assertTrue(accountObject.setPassword("test@1234", "test@123"));
+		assertFalse(accountObject.setPassword("test", "test"));
+	}
+
+	@Test
+	public void validatePasswordTest() {
+		assertFalse(accountObject.validatePassword(""));
+		assertFalse(accountObject.validatePassword("test@1234"));
+	}
+
+	@Test
+	public void hasPasswordTest() {
+		assertFalse(accountObject.hasPassword());
+		accountObject.removePassword();
+	}
+
+	@Test(expected = Exception.class)
+	public void setNamesTest() throws Exception {
+		assertNotNull(accountObject.setName(null));
+		assertNotNull(accountObject.setName(""));
+	}
+
+	@Test
+	public void getNames() {
+		assertNotNull(accountObject.setName("user1"));
+		assertNotNull(accountObject.setName("user2"));
+		assertNotNull(accountObject.setName("user3"));
+		assertNotNull(accountObject.getNames());
+		accountObject.removeName("user3");
+		assertFalse(accountObject.setUniqueName("user1"));
+		assertTrue(accountObject.setUniqueName("AdminUser"));
+	}
+}

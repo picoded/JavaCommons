fb0b26cb8dc3cfb69963fcc6dff03f0f8003b562
Rashmi Singh <rashmis19oct@gmail.com>
2017-01-12 18:52:04 +0530

    T1163  - JSql.struct - code coverage and migration

--- test/picoded/JSql/struct/AccountTable_Sqlite_test.java
+++ test/picoded/JSql/struct/AccountTable_Sqlite_test.java
@@ -3,14 +3,10 @@ package picoded.JSql.struct;
 // Target test class
 import picoded.TestConfig;
 import picoded.JSql.*;
-import picoded.JSql.struct.*;
 import picoded.JStruct.*;
-import picoded.JStruct.internal.*;
 
 // Test Case include
 import org.junit.*;
-
-import static org.junit.Assert.*;
 
 // Test depends
 import java.lang.String;
@@ -32,4 +28,34 @@ public class AccountTable_Sqlite_test extends AccountTable_test {
 		return jsObj.getAccountTable("AT_" + tableName);
 	}
 	
+	@Test
+	public void implementationDefaultConstructor() {
+		JSqlStruct jsObj = new JSqlStruct();
+		jsObj.sqlObj = sqlImplmentation();
+		jsObj.getAccountTable("AT_" + tableName);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setupAccountTableTest() {
+		JSqlStruct jsObj = new JSqlStruct();
+		jsObj.setupAccountTable("AT_" + tableName);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setupKeyValueMapTest() {
+		JSqlStruct jsObj = new JSqlStruct();
+		jsObj.setupKeyValueMap("AT_" + tableName);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setupAtomicLongMapTest() {
+		JSqlStruct jsObj = new JSqlStruct();
+		jsObj.setupAtomicLongMap("AT_" + tableName);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setupMetaTableTest() {
+		JSqlStruct jsObj = new JSqlStruct();
+		jsObj.setupMetaTable("AT_" + tableName);
+	}
 }
--- test/picoded/JSql/struct/KeyValueMap_Sqlite_test.java
+++ test/picoded/JSql/struct/KeyValueMap_Sqlite_test.java
@@ -1,9 +1,15 @@
 package picoded.JSql.struct;
 
 // Target test class
+import java.util.HashSet;
+
+import org.junit.Test;
+
 import picoded.TestConfig;
 import picoded.JSql.*;
 import picoded.JStruct.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 // Test Case include
 
@@ -24,4 +30,74 @@ public class KeyValueMap_Sqlite_test extends KeyValueMap_test {
 		return jsObj.getKeyValueMap("KVM_" + tableName);
 	}
 	
+	@Test
+	public void systemTeardownExceptionTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.systemTeardown();
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void maintenanceExceptionTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.maintenance();
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void getExpiryRawTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.getExpiryRaw("");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setExpiryRawTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.setExpiryRaw("", 1l);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void getValueRawTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.getValueRaw("", 1l);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void setValueRawTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.setValueRaw("", "", 1l);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void getKeysExceptionTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.getKeys("");
+	}
+	
+	@Test
+	public void getKeysNullKeyTest() {
+		kvmObj.put("yes", "no");
+		kvmObj.put("hello", "world");
+		kvmObj.put("this", "world");
+		kvmObj.put("is", "sparta");
+		assertNotNull(kvmObj.getKeys(null));
+	}
+	
+	@Test
+	public void getKeysNullKeyNullValueTest() {
+		assertEquals(new HashSet<String>(), kvmObj.getKeys(null));
+	}
+	
+	@Test
+	public void keySetTest() {
+		kvmObj.put("yes", "no");
+		kvmObj.put("hello", "world");
+		kvmObj.put("this", "world");
+		kvmObj.put("is", "sparta");
+		assertNotNull(kvmObj.keySet());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void removeTest() {
+		JSql_KeyValueMap jsObj = new JSql_KeyValueMap(sqlImplmentation(), "");
+		jsObj.remove("");
+	}
 }

00ad7e1f2e30eca623bd99cab17f7ba6ea3aab9c
Rashmi Singh <rashmis19oct@gmail.com>
2017-01-16 13:31:22 +0530

    T1163  - JSql.struct - code coverage and migration

--- test/picoded/JSql/struct/MetaTable_Mysql_test.java
+++ test/picoded/JSql/struct/MetaTable_Mysql_test.java
@@ -1,6 +1,8 @@
 package picoded.JSql.struct;
 
 // Target test class
+import org.junit.Test;
+
 import picoded.TestConfig;
 import picoded.JSql.*;
 
@@ -14,4 +16,10 @@ public class MetaTable_Mysql_test extends MetaTable_Sqlite_test {
 		return JSql.mysql(TestConfig.MYSQL_CONN_JDBC(), TestConfig.MYSQL_CONN_PROPS());
 	}
 	
+	@Test(expected = RuntimeException.class)
+	public void systemSetupTest() {
+		JSql_MetaTable jsObj = new JSql_MetaTable(sqlImplmentation(), "");
+		jsObj.sqlObj = null;
+		jsObj.systemSetup();
+	}
 }

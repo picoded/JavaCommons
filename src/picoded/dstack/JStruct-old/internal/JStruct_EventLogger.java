cc98eab3de2517650cdc4c53e5c6a269a70e7d90
Eugene Cheah <eugene@picoded.com>
2017-03-28 19:32:04 +0800

    Clearing out old JStruct files

--- src/picoded/dstack/JStruct-old/internal/JStruct_EventLogger.java
+++
@@ -1,9 +0,0 @@
-package picoded.JStruct.internal;
-
-import picoded.JStruct.EventLogger;
-
-/// Refence implementation of EventLogger data structure
-/// @TODO : Actually implement instead of proxying the logger
-public class JStruct_EventLogger implements EventLogger {
-	
-}

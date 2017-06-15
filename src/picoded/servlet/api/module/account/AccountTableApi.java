f523dcdd206d8fee04266797363e5ee39f22cc48
Eugene Cheah <eugene@picoded.com>
2017-06-15 12:48:30 +0800

    WIP

--- src/picoded/servlet/api/module/account/AccountTableApi.java
+++ src/picoded/servlet/api/module/account/AccountTableApi.java
@@ -9,5 +9,16 @@ import picoded.dstack.*;
 /// Account table API builder
 ///
 public class AccountTableApi implements ApiModule {
+
+	/// The AccountTable reference
+	protected AccountTable table = null;
+
+	/// Setup the account table api class
+	///
+	/// @param  The input AccountTable to use
+	public AccountTableApi(AccountTable inTable) {
+		table = inTable;
+	}
+
 	
 }

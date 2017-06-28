ead4060720f74b4cd2f1329fb99effd62c149be6
Jia Ming <jiaming@picoded.com>
2017-06-28 18:22:32 +0800

    Finished Login Process Flow and Added Login Throttling Test

--- test/picoded/servlet/api/module/account/AccountTableApi_test.java
+++ test/picoded/servlet/api/module/account/AccountTableApi_test.java
@@ -1,9 +1,13 @@
 package picoded.servlet.api.module.account;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.*;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
 
 import picoded.servlet.*;
 import picoded.servlet.api.*;
@@ -15,7 +19,7 @@ import picoded.dstack.module.account.*;
 
 /// Test the AccountTable API specifically
 public class AccountTableApi_test extends ApiModule_test {
-	
+
 	//----------------------------------------------------------------------------------------
 	//
 	//  Test setup
@@ -26,9 +30,13 @@ public class AccountTableApi_test extends ApiModule_test {
 	public static class AccountTableApiTestServlet extends ApiModuleTestServlet {
 		@Override
 		public ApiModule moduleSetup(CommonStack stack) {
+			System.out.println("ModuleSetup was ran.");
 			AccountTable table = new AccountTable(stack, "account");
 			AccountTableApi ret = new AccountTableApi(table);
-
+			// table.loginThrottle = (inAo, failures) -> {
+			// 	System.out.println("this was not ran");
+			// 	return (long) 2;
+			// };
 			if( !table.hasLoginID("laughing-man") ) {
 				AccountObject ao = table.newObject("laughing-man");
 				ao.setPassword("The Catcher in the Rye");
@@ -48,14 +56,14 @@ public class AccountTableApi_test extends ApiModule_test {
 	//
 	//----------------------------------------------------------------------------------------
 
-	@Test 
+	@Test
 	public void isLoginFailure() {
 		assertNull( requestJSON("isLogin", null).get("INFO") );
 		assertNull( requestJSON("isLogin", null).get("ERROR") );
 		assertEquals( Boolean.FALSE, requestJSON("isLogin", null).get("return") );
 	}
 
-	@Test 
+	@Test
 	public void loginProcessFlow() {
 		// reuse result map
 		Map<String,Object> res = null;
@@ -84,8 +92,50 @@ public class AccountTableApi_test extends ApiModule_test {
 		assertNull( res.get("ERROR") );
 		assertNull( res.get("INFO") );
 		assertEquals( Boolean.TRUE, res.get("isLogin") );
-
 		// Validate that login is valid
-		// assertEquals( Boolean.TRUE, requestJSON("isLogin", null).get("return") );
+		res = requestJSON("isLogin", null);
+		assertEquals( Boolean.TRUE, res.get("return") );
+	}
+
+	@Test
+	public void loginLockingIncrement() {
+		// reuse result map
+		Map<String,Object> res = null;
+		// Checks that the test begins with the user not logged in
+		res = requestJSON("isLogin", null);
+		assertEquals( Boolean.FALSE, res.get("return") );
+
+		Map<String,Object> loginParams = new HashMap<String,Object>();
+		loginParams.put("loginID", "laughing-man");
+		loginParams.put("loginPass", "Is the enemy");
+		int doTestLoop = 3, currentLoop = 0;
+		while(currentLoop < doTestLoop){
+			res = requestJSON("login", loginParams);
+			// System.out.println(res.get("ERROR")+" <<<<<<<<<<");
+			Map<String, Object> params = new HashMap<String,Object>();
+			params.put("accountName", "laughing-man");
+			int waitTime = (int) requestJSON("lockTime", params).get("lockTime");
+			if(currentLoop%2==0){
+				assertEquals( Boolean.FALSE, res.get("isLogin") );
+				assertEquals("It failed on the Loop Number: "+ (currentLoop+1) +" with waitTime: "+waitTime+"\n"+
+											"Likely failure is due to insufficient Thread.sleep()\n",
+											"Failed login (wrong password or invalid user?)", res.get("ERROR") );
+			}else{
+				assertThat("It failed on the Loop Number: "+ (currentLoop+1) +" with waitTime: "+waitTime+"\n",
+				 						res.get("ERROR").toString(), containsString("user locked out") );
+				try{
+					Thread.sleep(waitTime*1000);
+				}catch(InterruptedException ie){}
+			}
+			currentLoop++;
+		}
+	}
+
+	@Test
+	public void groupFunctionalities(){
+		// reuse result map
+		Map<String,Object> res = null;
+		res = requestJSON("membershipRoles", null);
+		assertNotNull(res.get("list"));
 	}
 }

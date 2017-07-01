250c845cb0d22277b09736198efeb0b13c05d8ba
Eugene Cheah <eugene@picoded.com>
2017-07-02 01:34:26 +0800

    JSQL experimental random select support

--- src/picoded/dstack/jsql/connector/JSql.java
+++ src/picoded/dstack/jsql/connector/JSql.java
@@ -1116,7 +1116,6 @@ public abstract class JSql {
 		//
 		String indexSuffix // The index name suffix, its auto generated if null
 	) {
-		
 		if (tableName.length() > 30) {
 			LOGGER.warning(JSqlException.oracleNameSpaceWarning + tableName);
 		}
@@ -1154,4 +1153,152 @@ public abstract class JSql {
 		return new JSqlPreparedStatement(queryBuilder.toString(), queryArgs.toArray(), this);
 	}
 	
+
+
+	//-------------------------------------------------------------------------
+	//
+	// Randomly SELECT statement builder 
+	//
+	//-------------------------------------------------------------------------
+	
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// See : randomSelectStatement for full docs
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	/// @param  Where statement to filter  (eg: col1=?)
+	/// @param  Where arguments value      (eg: [value/s])
+	///
+	/// @return  The JSqlResult
+	public JSqlResult randomSelect( //
+		String tableName, // Table name to select from
+		String selectStatement // The Columns to select, null means all
+	) {
+		return randomSelect(tableName, selectStatement, null, null, 0);
+	}
+
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// See : randomSelectStatement for full docs
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	/// @param  Where statement to filter  (eg: col1=?)
+	/// @param  Where arguments value      (eg: [value/s])
+	///
+	/// @return  The JSqlResult
+	public JSqlResult randomSelect( //
+		String tableName, // Table name to select from
+		//
+		String selectStatement, // The Columns to select, null means all
+		//
+		String whereStatement, // The Columns to apply where clause, this must be sql neutral
+		Object[] whereValues // Values that corresponds to the where statement
+	) {
+		return randomSelect(tableName, selectStatement, whereStatement, whereValues, 0);
+	}
+
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// See : randomSelectStatement for full docs
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	/// @param  Where statement to filter  (eg: col1=?)
+	/// @param  Where arguments value      (eg: [value/s])
+	/// @param  Row count, 0 for all       (eg: 3)
+	///
+	/// @return  The JSqlResult
+	public JSqlResult randomSelect( //
+		String tableName, // Table name to select from
+		//
+		String selectStatement, // The Columns to select, null means all
+		//
+		String whereStatement, // The Columns to apply where clause, this must be sql neutral
+		Object[] whereValues, // Values that corresponds to the where statement
+		//
+		long rowLimit // Number of rows
+	) {
+		return randomSelectStatement(tableName, selectStatement, whereStatement, whereValues, rowLimit).query();
+	}
+
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// See : randomSelectStatement for full docs
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	///
+	/// @return  A prepared SELECT statement
+	public JSqlPreparedStatement randomSelectStatement( //
+		String tableName, // Table name to select from
+		String selectStatement // The Columns to select, null means all
+	) {
+		return randomSelectStatement(tableName, selectStatement, null, null, 0);
+	}
+
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// See : randomSelectStatement for full docs
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	/// @param  Where statement to filter  (eg: col1=?)
+	/// @param  Where arguments value      (eg: [value/s])
+	///
+	/// @return  A prepared SELECT statement
+	public JSqlPreparedStatement randomSelectStatement( //
+		String tableName, // Table name to select from
+		//
+		String selectStatement, // The Columns to select, null means all
+		//
+		String whereStatement, // The Columns to apply where clause, this must be sql neutral
+		Object[] whereValues // Values that corresponds to the where statement
+	) {
+		return randomSelectStatement(tableName, selectStatement, whereStatement, whereValues, 0);
+	}
+
+	/// Helps generate an random SQL SELECT request. This function was created to acommedate the various
+	/// syntax differances of SELECT across the various SQL vendors.
+	///
+	/// SEE: https://stackoverflow.com/questions/580639/how-to-randomly-select-rows-in-sql
+	///
+	/// The syntax below, is an example of such a random SELECT statement for SQLITE.
+	///
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.SQL}
+	/// SELECT
+	///	col1, col2      // select collumn
+	/// FROM tableName    // table name to select from
+	/// WHERE
+	///	col1=?          // where clause
+	/// ORDER BY RANDOM() // Random sorting
+	/// LIMIT 1           // number of rows to fetch
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	///
+	/// @param  Table name to query        (eg: tableName)
+	/// @param  Columns to select          (eg: col1, col2)
+	/// @param  Where statement to filter  (eg: col1=?)
+	/// @param  Where arguments value      (eg: [value/s])
+	/// @param  Row count, 0 for all       (eg: 3)
+	///
+	/// @return  A prepared SELECT statement
+	public JSqlPreparedStatement randomSelectStatement( //
+		String tableName, // Table name to select from
+		//
+		String selectStatement, // The Columns to select, null means all
+		//
+		String whereStatement, // The Columns to apply where clause, this must be sql neutral
+		Object[] whereValues, // Values that corresponds to the where statement
+		//
+		long rowLimit // Number of rows
+	) {
+		throw new UnsupportedOperationException(JSqlException.invalidDatabaseImplementationException);
+	}
+
 }

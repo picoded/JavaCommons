f2c6bbfdd1e2cca34a4004fb8db2e5538a68ba0b
Eugene Cheah <eugene@picoded.com>
2017-07-28 17:38:59 +0800

    More bench testing options

---
+++ test/picoded/dstack/jsql/perf/JSqlClob_Mysql_perf.java
@@ -0,0 +1,7 @@
+package picoded.dstack.jsql.perf;
+
+/// [Mysql varient]
+/// Testing of metatable full CLOB structure performance
+public class JSqlClob_Mysql_perf extends JSqlClob_perf {
+
+}
---
+++ test/picoded/dstack/jsql/perf/JSqlFixedIndexless_Mysql_perf.java
@@ -0,0 +1,7 @@
+package picoded.dstack.jsql.perf;
+
+/// [Mysql varient]
+/// Testing of metatable full indexless fixed table performance
+public class JSqlFixedIndexless_Mysql_perf extends JSqlFixedIndexless_perf {
+
+}

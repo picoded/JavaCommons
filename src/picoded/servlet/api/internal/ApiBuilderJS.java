45dabdff439298881298e5350c0d2b9371637bf6
Rashmi Singh <rashmis19oct@gmail.com>
2017-08-01 12:39:41 +0530

    T1660 - fixed the changes for Enable Reading of .js files from ApiBuilderJS.java

--- src/picoded/servlet/api/internal/ApiBuilderJS.java
+++ src/picoded/servlet/api/internal/ApiBuilderJS.java
@@ -1,13 +1,29 @@
 package picoded.servlet.api.internal;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ApiBuilderJS {
 
 	public static String getJsLib() {
-		// Somehow read and get the ApiBuilderJS.js file
-		// in MULTIPLE SITUATIONS
-		//
-		// Compiled inside a JAR
-		// Compiled as per normal
+		Scanner scanner = null;
+		try {
+			File file = new File(ApiBuilderJS.class.getResource("ApiBuilderJS.js").getPath());
+			StringBuilder fileContents = new StringBuilder((int) file.length());
+			scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				fileContents.append(scanner.nextLine()
+						+ System.getProperty("line.separator"));
+			}
+			return fileContents.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(scanner !=null){
+				scanner.close();
+			}
+		}
+		return null;
 	}
-
 }

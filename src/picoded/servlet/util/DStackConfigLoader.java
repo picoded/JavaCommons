8ee8718f97328c2bf6c976a6f3cc7645fe36801f
Jia Ming <jiaming@picoded.com>
2017-08-03 16:21:21 +0800

    Added DStackConfigLoader.java

---
+++ src/picoded/servlet/util/DStackConfigLoader.java
@@ -0,0 +1,42 @@
+package picoded.servlet.util;
+
+import picoded.dstack.DStack;
+import picoded.dstack.jsql.JSqlStack;
+import picoded.dstack.jsql.connector.JSql;
+import picoded.dstack.CommonStack;
+
+import picoded.struct.GenericConvertMap;
+import picoded.conv.ConvertJSON;
+
+public class DStackConfigLoader {
+  public DStackConfigLoader(){
+
+  }
+
+  public static CommonStack configStringToCommonStack ( String config ) {
+    GenericConvertMap<String, Object> configMap = ConvertJSON.toCustomClass(config, GenericConvertMap.class);
+    if ( configMap == null )
+      return null;
+    String type = configMap.getString("type");
+
+    if ( type.equalsIgnoreCase("Jsql") ) {
+			String engine = configMap.getString("engine", "");
+			String path = configMap.getString("path", "");
+			String username = configMap.getString("username", "");
+			String password = configMap.getString("password", "");
+			String database = configMap.getString("database", "");
+      if ( path.isEmpty() )
+        throw new RuntimeException("DStack path is not set.");
+      if ( engine.equalsIgnoreCase("sqlite") ) {
+        return  new JSqlStack(JSql.sqlite(path));
+      } else if ( egine.equalsIgnoreCase("mysql") ) {
+        return new JSqlStack(Jsql.mysql(path, database, username, password));
+      }
+      return null;
+    }
+  }
+
+  // public static DStack generateDStack ( String config ) {
+  //
+  // }
+}

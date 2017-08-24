543c16a02753f9e104151bb29c59e2f8907c2fed
Eugene Cheah <eugene@picoded.com>
2017-08-24 12:45:26 +0800

    WIP DataTableApi

---
+++ src/picoded/servlet/api/module/dstack/DataTableApi.java
@@ -0,0 +1,89 @@
+package picoded.serlvet.api.module.dstack;
+
+import static picoded.serlvet.api.module.dstack.DStackApiConstantStrings.*;
+
+/**
+ * Does the constructor setup of DataTable
+ */
+public class DataTableApi extends AbstractApiModule {
+
+	/////////////////////////////////////////////
+	//
+	// Constructor setup
+	//
+	/////////////////////////////////////////////
+	
+	// Internal data table object, set by constructor
+	protected DataTable dataTableObj = null;
+
+	/**
+	 * Constructor for DataTableApi
+	 *
+	 * @param  DataTable  the data table object for the API to build on
+	 */
+	public DataTableApi(DataTable inTable) {
+		dataTableObj = inTable;
+	}
+
+	/////////////////////////////////////////////
+	//
+	// Getter functions
+	//
+	/////////////////////////////////////////////
+	
+	/** 
+	 * # $prefix/get
+	 *
+	 * Gets and return the data object
+	 *
+	 * ## HTTP Request Parameters
+	 *
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | Parameter Name  | Variable Type	    | Description                                                                   |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | _oid            | String             | object ID used to retrieve the meta object. If no oid is given, return null.  |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | stringEscape    | Boolean (optional) | Default TRUE. If false, returns UNSANITISED data, so common HTML escape       |
+	 * |                 |                    | characters are returned as well.                                              |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 *
+	 * ## JSON Object Output Parameters
+	 *
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | Parameter Name  | Variable Type	    | Description                                                                   |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | _oid            | String             | The internal object ID used (or created)                                      |
+	 * | stringEscape    | Boolean            | Indicate if string escape occured                                             |
+	 * | data            | {Object}           | Meta object that represents this account                                      |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	 * | error           | String (Optional)  | Errors encounted if any                                                       |
+	 * +-----------------+--------------------+-------------------------------------------------------------------------------+
+	*/
+	public ApiFunction dataGet = (req, res) -> {
+		// Get the oid, and sanatise output settings
+		String oid = req.getString(OID, null);
+		boolean stringEscape = req.getBoolean(STRING_ESCAPE, true);
+		
+		// Put back config in response
+		res.put(OID, oid);
+		res.put(STRING_ESCAPE, stringEscape);
+		
+		// Try get the object respectively
+		try {
+			// DataObject 
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		return res;
+	};
+	
+	/////////////////////////////////////////////
+	//
+	// Utility functions
+	//
+	/////////////////////////////////////////////
+	
+
+
+}
